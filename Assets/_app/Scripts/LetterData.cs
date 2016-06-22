c01c2fcfc2215c722670e0ab2b3a288b9c6687f4
Brago <bragolab@gmail.com>
2016-06-22 17:38:07 +0200

    # Antura - v.0.1.1.0 #
    - Added structure app Antura.
    - Added games structure.

---
+++ Assets/_app/Scripts/LetterData.cs
@@ -0,0 +1,21 @@
+﻿using Google2u;
+
+namespace Test_00 {
+    public class LetterData {
+        public string Key;
+        public string Isolated_Unicode;
+        public string Isolated;
+        public string Initial_Unicode;
+        public string Medial_Unicode;
+        public string Final_Unicode;
+
+        public LetterData(string _keyRow, lettersRow _letRow) {
+            Key = _keyRow;
+            Isolated = _letRow._isolated;
+            Isolated_Unicode = _letRow._unicode;
+            Initial_Unicode = _letRow._initial_unicode;
+            Medial_Unicode = _letRow._medial_unicode;
+            Final_Unicode = _letRow._final_unicode;
+        }
+    }
+}
