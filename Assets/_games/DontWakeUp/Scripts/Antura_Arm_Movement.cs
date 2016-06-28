215d0e1f9d96b0438c4836ca2dcbc107cd2f1b0f
Stefano Cecere <stefano.cecere@krur.com>
2016-06-28 12:42:15 +0200

    basic project reorganization

---
+++ Assets/_games/DontWakeUp/Scripts/Antura_Arm_Movement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class Antura_Arm_Movement : MonoBehaviour
+{
+
+    public float Delay;
+    public float Duration;
+    public Vector3 DestinationAngle;
+    public bool FastBeyod360;
+
+    void OnEnable()
+    {
+
+        Sequence mySequence = DOTween.Sequence();
+        mySequence.AppendInterval(Delay)
+                  .Append(transform.DOLocalRotate(DestinationAngle, Duration, (FastBeyod360 ? RotateMode.FastBeyond360 : RotateMode.Fast)))
+                  .SetLoops(-1, LoopType.Yoyo)
+        ;
+
+        //DOLocalRotate(Vector3 to, float duration, RotateMode mode);
+
+        // DOTween.To(() => myValue, x => myValue = x, 0, 3).SetOptions(true).SetDelay(1);
+
+        /*                DOTween.Sequence()
+                                   .Append(transform.DOMoveX(_closedX, 0.2f, false).SetEase(Ease.Linear))
+                                   .AppendCallback(ChangeTextDirect)
+                                   .Append(transform.DOMoveX(0.0f, 0.5f, false).SetEase(Ease.Linear));*/
+
+
+    }
+
+}
