2f86d9bbb6ab314a7e4a11e382a08e7c4d94ef77
George H <george.hbr@gmail.com>
2016-07-04 21:42:19 +0300

    Balloons game prototype

---
+++ Assets/_games/Balloons/Scripts/BalloonTopController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloonTopController : MonoBehaviour
+{
+    public BalloonController parent;
+    public Collider balloonCollider;
+
+    private Rigidbody body;
+    private Animator animator;
+    private AudioSource popAudio;
+    private int taps = 0;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        popAudio = GetComponent<AudioSource>();
+    }
+
+    public void OnMouseDown()
+    {
+        TapAction();
+    }
+
+    void TapAction()
+    {
+        taps++;
+        if (taps >= parent.tapsNeeded)
+        {
+            Pop();
+        }
+    }
+
+    public void Pop()
+    {
+        balloonCollider.enabled = false;
+        parent.Pop();
+        popAudio.Play();
+        animator.SetBool("Pop", true);
+    }
+
+    //    void Shrink()
+    //    {
+    //        transform.localScale = new Vector3(transform.localScale.x - 0.25f, transform.localScale.y - 0.25f, transform.localScale.z - 0.25f);
+    //    }
+}
