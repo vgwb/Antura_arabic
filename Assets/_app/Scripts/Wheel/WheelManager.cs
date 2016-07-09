81c2b9adb37438c9d3247d35d5814a77825ed452
Stefano Cecere <stefano.cecere@krur.com>
2016-07-09 22:57:00 +0200

    Wheel colors and sound manager

---
+++ Assets/_app/Scripts/Wheel/WheelManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace EA4S
+{
+    public class WheelManager : MonoBehaviour
+    {
+        public static WheelManager Instance;
+        public GameObject Popup;
+        public GameObject GameIcon;
+
+        int currentSector;
+        Image PopupImage;
+
+        void Awake() {
+            Instance = this;
+            currentSector = 0;
+            PopupImage = Popup.GetComponent<Image>();
+        }
+
+        void Start() {
+            GameIcon.SetActive(false);
+        }
+
+        public void OnWheenStopped() {
+            GameIcon.SetActive(true);
+        }
+
+        public void OnRadiusTrigger(int number, Color _color) {
+            if (number != currentSector) {
+                currentSector = number;
+                //Debug.Log("OnRadiusTrigger" + currentSector);
+
+                PopupImage.color = _color;
+
+                AudioManager.PlaySound("SFX/hit");
+            }
+
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+	
+        }
+    }
+}
