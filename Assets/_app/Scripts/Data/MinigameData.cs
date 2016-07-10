9b0858fe2810cb69a9ca2f2fb496512529db7fdc
Stefano Cecere <stefano.cecere@krur.com>
2016-07-10 10:22:38 +0200

    WHeel: labels, animation, audio mixer, game icons

---
+++ Assets/_app/Scripts/Data/MinigameData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EA4S
+{
+    public class MinigameData
+    {
+        public string Code;
+        public string Title;
+        public string EnglishTitle;
+        public bool Available;
+
+        public MinigameData(string code, string title, string englishTitle, bool available) {
+            Code = code;
+            Title = title;
+            EnglishTitle = englishTitle;
+            Available = available;
+        }
+
+        public string GetIconResourcePath() {
+            return "Images/GameIcons/minigame_icon_" + Code;
+        }
+
+    }
+}
--- Assets/_app/Scripts/Wheel/WheelManager.cs
+++ Assets/_app/Scripts/Wheel/WheelManager.cs
@@ -1,48 +1,66 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 using System.Collections;
+using System.Collections.Generic;
+using ArabicSupport;
+using DG.Tweening;
 
 namespace EA4S
 {
+
     public class WheelManager : MonoBehaviour
     {
         public static WheelManager Instance;
         public GameObject Popup;
         public GameObject GameIcon;
 
-        int currentSector;
+        public GameObject TutorialArrow;
+        public GameObject GameTitle;
+
+        public List<MinigameData> gameData = new List<MinigameData>();
+
+        int currentGameIndex;
         Image PopupImage;
+
+        int gameCounter;
+        TextMeshProUGUI labelText;
 
         void Awake() {
             Instance = this;
-            currentSector = 0;
+            currentGameIndex = 0;
             PopupImage = Popup.GetComponent<Image>();
+            labelText = GameTitle.GetComponent<TextMeshProUGUI>();
+
+            gameData.Add(new MinigameData("fastcrowd", "الحشد سريع", "Fast Crowd", true));
+            gameData.Add(new MinigameData("dontwakeup", "لا يستيقظون", "Don't Wake Up", true));
+            gameData.Add(new MinigameData("balloons", "بالونات", "Balloons", true));
+            gameData.Add(new MinigameData("pianowoof", "بيانو", "Piano Woof", true));
         }
 
         void Start() {
-            GameIcon.SetActive(false);
+            //GameIcon.SetActive(false);
         }
 
         public void OnWheenStopped() {
-            GameIcon.SetActive(true);
+            GameIcon.GetComponent<Image>().sprite = Resources.Load<Sprite>(gameData[currentGameIndex].GetIconResourcePath());
+            //GameIcon.SetActive(true);
         }
 
         public void OnRadiusTrigger(int number, Color _color) {
-            if (number != currentSector) {
-                currentSector = number;
+            if (number != currentGameIndex) {
+                currentGameIndex = (number % 4);
                 //Debug.Log("OnRadiusTrigger" + currentSector);
 
                 PopupImage.color = _color;
 
+                labelText.text = ArabicFixer.Fix(gameData[currentGameIndex].Title, false, false);
+                GameIcon.GetComponent<Image>().sprite = Resources.Load<Sprite>(gameData[currentGameIndex].GetIconResourcePath());
                 AudioManager.PlaySound("SFX/hit");
             }
 
 
         }
 
-        // Update is called once per frame
-        void Update() {
-	
-        }
     }
 }
