de55f296905a813f2768c307808818eda6a258ea
Stefano Cecere <stefano.cecere@krur.com>
2016-07-10 23:58:00 +0200

    WalkieTalkie

--- Assets/_app/Scripts/Wheel/WheelManager.cs
+++ Assets/_app/Scripts/Wheel/WheelManager.cs
@@ -45,12 +45,7 @@ namespace EA4S
 
         void Start() {
             isGameSelected = false;
-            //AudioManager.I.PlaySound("Music/play");
             AudioManager.I.PlayMusic("Music1");
-            //AudioManager.I.PlayMusic();
-
-            //ShakePopup();
-
         }
 
         public void OnPopuplicked() {
@@ -60,16 +55,16 @@ namespace EA4S
         }
 
         void ShakePopup() {
+//            Sequence mySequence = DOTween.Sequence();
+//            mySequence.Append(transform.DOMoveX(45, 1))
+//                .Append(transform.DORotate(new Vector3(0,180,0), 1))
+//                 .PrependInterval(1)
+//                  .Insert(0, transform.DOScale(new Vector3(3,3,3), mySequence.Duration()));
+//            
             Tween BobiBobi = Popup.GetComponent<RectTransform>().DOPunchScale(new Vector2(0.2f, 0.2f), 1.0f, 5, 1f)
                 .SetLoops(0).SetUpdate(true).SetAutoKill(true)
                 .SetEase(Ease.InOutQuad).Play();
-
-            //BobiBobi = PopupRect.DOSizeDelta(new Vector2(5, 5), 2f).SetRelative().SetUpdate(true).SetAutoKill(false).SetEase(Ease.InOutQuad).SetLoops(-1, LoopType.Yoyo);
-
-            //            BobiBobi = Popup.GetComponent<RectTransform>().DORotate(new Vector3(0, 0, -20), 0.6f).SetRelative().SetUpdate(true).SetAutoKill(false)
-            //                .SetEase(Ease.InOutQuad).SetLoops(2, LoopType.Yoyo).Play();
         }
-
 
         public void OnWheelStart() {
             TutorialArrow.SetActive(false);
