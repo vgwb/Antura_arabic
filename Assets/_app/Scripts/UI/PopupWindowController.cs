848178de0787bfe973503d3be0e30272b4e0e530
Stefano Cecere <stefano.cecere@krur.com>
2016-07-12 16:33:52 +0200

    imprioved again PopupController

--- Assets/_app/Scripts/UI/PopupWindowController.cs
+++ Assets/_app/Scripts/UI/PopupWindowController.cs
@@ -1,19 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 using TMPro;
+using ArabicSupport;
 
 public class PopupWindowController : MonoBehaviour
 {
 
     public static PopupWindowController I;
     public GameObject TitleGO;
+    public GameObject DrawingContainerGO;
     public GameObject DrawingImageGO;
+    public GameObject WordContainerGO;
     public GameObject WordTextGO;
     public GameObject ButtonGO;
 
     void Start() {
         I = this;
     }
-	
 
+    public void SetTitle(string text) {
+        TitleGO.GetComponent<TextMeshProUGUI>().text = ArabicFixer.Fix(text, false, false);
+    }
+
+    public void SetWord(string word) {
+        // here set both word and drawing 
+    }
+
+    public void OnPressButton() {
+        
+    }
 }
