005557ee87eedd6e4bec5b07027a6b26526d65e5
Stefano Cecere <stefano.cecere@krur.com>
2016-07-12 17:50:19 +0200

    Test Home Screen to launch testmode

---
+++ Assets/_app/Scripts/Test/TestHomeManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TestHomeManager : MonoBehaviour
+{
+
+
+
+    void Start() {
+	
+    }
+
+    public void StartTest() {
+        SceneManager.LoadScene("Wheel");
+    }
+}
