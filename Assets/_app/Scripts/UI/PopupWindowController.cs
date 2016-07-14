3f35afad10f1279de7d8f21174ad1646a33270d1
Stefano Cecere <stefano.cecere@krur.com>
2016-07-14 12:32:39 +0200

    DontWakeUp levels and omse UI

--- Assets/_app/Scripts/UI/PopupWindowController.cs
+++ Assets/_app/Scripts/UI/PopupWindowController.cs
@@ -9,6 +9,9 @@ namespace EA4S
     {
 
         public static PopupWindowController I;
+
+        public GameObject Manager;
+
         public GameObject TitleGO;
         public GameObject DrawingImageGO;
         public GameObject WordTextGO;
@@ -18,16 +21,26 @@ namespace EA4S
             I = this;
         }
 
+        public void Init(string introText, string word) {
+            SetTitle(introText);
+            SetWord(word);
+            
+        }
+
         public void SetTitle(string text) {
             TitleGO.GetComponent<TextMeshProUGUI>().text = ArabicFixer.Fix(text, false, false);
         }
 
         public void SetWord(string word) {
             // here set both word and drawing 
+
+            WordTextGO.GetComponent<TextMeshProUGUI>().text = ArabicFixer.Fix(word, false, false);
+
         }
 
         public void OnPressButton() {
-        
+            Manager.SendMessage("PopupPressedContinue");
+            gameObject.SetActive(false);
         }
     }
 }
