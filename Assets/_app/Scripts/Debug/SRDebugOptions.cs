36e5c1ea48dc50b5d892f935535bdb26c7ef63dd
Stefano Cecere <stefano.cecere@krur.com>
2016-07-14 22:50:32 +0200

    Added SRDebugger Plugin for ingame debug and options (triple click top left corner to activate)

---
+++ Assets/_app/Scripts/Debug/SRDebugOptions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public partial class SROptions
+{
+    [Category("Open Minigames")]
+    public void FastCrowd() {
+        //Debug.Log("Clearing PlayerPrefs");
+        SceneManager.LoadScene("game_FastCrowd");
+        SRDebug.Instance.HideDebugPanel();
+    }
+
+    [Category("Open Minigames")]
+    public void DontWakeUp() {
+        //Debug.Log("Clearing PlayerPrefs");
+        SceneManager.LoadScene("game_DontWakeUp");
+        SRDebug.Instance.HideDebugPanel();
+    }
+
+    [Category("Open Minigames")]
+    public void Balloons() {
+        //Debug.Log("Clearing PlayerPrefs");
+        SceneManager.LoadScene("game_Balloons");
+        SRDebug.Instance.HideDebugPanel();
+    }
+}
