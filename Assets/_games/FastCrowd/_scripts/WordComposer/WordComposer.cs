adf02dcb14a38a9cfb76b885a0becc1f7abe502e
Brago <bragolab@gmail.com>
2016-07-21 17:06:33 +0200

    Fastcrowd : Letter out of gameplay can be reactivated.

---
+++ Assets/_games/FastCrowd/_scripts/WordComposer/WordComposer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EA4S { 
+
+    public class WordComposer : MonoBehaviour {
+
+	    // Use this for initialization
+	    void Start () {
+	
+	    }
+	
+	    // Update is called once per frame
+	    void Update () {
+	
+	    }
+
+        #region API
+        /// <summary>
+        /// .
+        /// </summary>
+        public void UpdateWord() {
+
+        }
+
+        #endregion
+    }
+}
