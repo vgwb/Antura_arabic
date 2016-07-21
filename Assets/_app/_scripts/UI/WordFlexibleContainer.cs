932e2d04bcc830333b16f98e7588f2cc8c99bb1b
Stefano Cecere <stefano.cecere@krur.com>
2016-07-21 18:38:45 +0200

    LivingLetter Flexible Container Prefab

---
+++ Assets/_app/_scripts/UI/WordFlexibleContainer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class WordFlexibleContainer : MonoBehaviour
+{
+
+    public TextMeshProUGUI Label;
+
+    void Start() {
+	
+    }
+
+    public void SetText(string label) {
+        Label.text = label;
+    }
+
+}
