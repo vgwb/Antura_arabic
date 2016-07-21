98dc9e45eecd8ecfbd42358276c1daf0cab72930
Brago <bragolab@gmail.com>
2016-07-22 01:32:10 +0200

    Fastcrowd: WordComposer, RightWord counter.

--- Assets/_games/FastCrowd/_scripts/WordComposer/WordComposer.cs
+++ Assets/_games/FastCrowd/_scripts/WordComposer/WordComposer.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 
 namespace EA4S { 
 
     public class WordComposer : MonoBehaviour {
 
+        public TextMeshProUGUI WordLable;
+        List<LetterData> CompletedLetters = new List<LetterData>();
+
 	    // Use this for initialization
 	    void Start () {
-	
-	    }
+            UpdateWord();
+        }
 	
 	    // Update is called once per frame
 	    void Update () {
@@ -20,7 +24,12 @@ namespace EA4S {
         /// 
         /// </summary>
         public void UpdateWord() {
-
+            string word = string.Empty;
+            foreach (LetterData letter in CompletedLetters) {
+                word += letter.Isolated;
+            }
+            word = ArabicAlphabetHelper.ParseWord(word,AppManager.Instance.Letters);
+            WordLable.text = word;
         }
 
         #endregion
@@ -28,11 +37,13 @@ namespace EA4S {
         #region event subscription delegates
 
         private void Droppable_OnRightMatch(LetterObjectView _letterView) {
+            CompletedLetters.Add(_letterView.Model.Data);
             UpdateWord();
         }
 
         private void DropContainer_OnObjectiveBlockCompleted() {
-            throw new System.NotImplementedException();
+            CompletedLetters = new List<LetterData>();
+            UpdateWord();
         }
 
         #endregion
@@ -41,13 +52,11 @@ namespace EA4S {
 
         void OnEnable() {
             DropContainer.OnObjectiveBlockCompleted += DropContainer_OnObjectiveBlockCompleted;
-
             Droppable.OnRightMatch += Droppable_OnRightMatch;
         }
         
         void OnDisable() {
             DropContainer.OnObjectiveBlockCompleted -= DropContainer_OnObjectiveBlockCompleted;
-
             Droppable.OnRightMatch -= Droppable_OnRightMatch;
         }
 
