59cebfd2e75532c2a5bf4771495990c95c928f33
Demigiant <daniele.giardini@gmail.com>
2016-07-27 11:43:38 +0200

    Added SceneTransitioner (script, prefab, images)

---
+++ Assets/_tests/Daniele/_Scripts/DTester.cs
@@ -0,0 +1,18 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+using UnityEngine;
+
+namespace EA4S
+{
+    /// <summary>
+    /// Just used to test stuff here and there. Not to be included in final build
+    /// </summary>
+    public class DTester : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                SceneTransitioner.Show(!SceneTransitioner.IsShown);
+            }
+        }
+    }
+}
