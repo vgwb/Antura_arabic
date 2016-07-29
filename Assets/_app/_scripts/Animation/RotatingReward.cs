87b5c0bf20835b51da3fdd6a413ce079faf69557
Stefano Cecere <stefano.cecere@krur.com>
2016-07-29 10:17:57 +0200

    Rewards scene

---
+++ Assets/_app/_scripts/Animation/RotatingReward.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingReward : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+
+    void Update() {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/_app/_scripts/Controllers/Rewards/RewardsManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using ModularFramework.Core;
+
+namespace EA4S
+{
+    public class RewardsManager : MonoBehaviour
+    {
+
+
+        void Start() {
+            ContinueScreen.Show(Continue, ContinueScreenMode.Button);
+        }
+
+        public void Continue() {
+            GameManager.Instance.Modules.SceneModule.LoadSceneWithTransition("app_Journey");
+        }
+
+    }
+}
