910a38dfa221740fa5e98b4cbc5565ca34126a95
Stefano Cecere <stefano.cecere@krur.com>
2016-07-29 15:28:27 +0200

    music in Rewards

--- Assets/_app/_scripts/Controllers/Rewards/RewardsManager.cs
+++ Assets/_app/_scripts/Controllers/Rewards/RewardsManager.cs
@@ -9,11 +9,13 @@ namespace EA4S
     {
 
 
-        IEnumerator Start()
-        {
+        IEnumerator Start() {
+            AudioManager.I.PlayMusic(Music.Theme4);
+
             // Wait for animation to complete
             RewardsAnimator animator = this.GetComponent<RewardsAnimator>();
-            while (!animator.IsComplete) yield return null;
+            while (!animator.IsComplete)
+                yield return null;
 
             ContinueScreen.Show(Continue, ContinueScreenMode.ButtonFullscreen);
         }
