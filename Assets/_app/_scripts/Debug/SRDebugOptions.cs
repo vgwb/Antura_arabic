a0567b2580b93d1f43acf59583c773f873faaed5
Stefano Cecere <stefano.cecere@krur.com>
2016-07-30 19:18:40 +0200

    [debug] fix jumping directly to scenes (closes popup window and new API)

--- Assets/_app/_scripts/Debug/SRDebugOptions.cs
+++ Assets/_app/_scripts/Debug/SRDebugOptions.cs
@@ -4,40 +4,45 @@ using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using EA4S;
+using ModularFramework.Core;
 
 public partial class SROptions
 {
     [Category("Open Scene")]
-    public void Home() {
-        //Debug.Log("Clearing PlayerPrefs");
-        SceneManager.LoadScene("app_Start");
+    public void Home()
+    {
+        WidgetPopupWindow.Close();
+        GameManager.Instance.Modules.SceneModule.LoadSceneWithTransition("app_Start");
         SRDebug.Instance.HideDebugPanel();
     }
 
     [Category("Open Scene")]
-    public void FastCrowd() {
-        //Debug.Log("Clearing PlayerPrefs");
-        SceneManager.LoadScene("game_FastCrowd");
+    public void FastCrowd()
+    {
+        WidgetPopupWindow.Close();
+        GameManager.Instance.Modules.SceneModule.LoadSceneWithTransition("game_FastCrowd");
         SRDebug.Instance.HideDebugPanel();
     }
 
     [Category("Open Scene")]
-    public void DontWakeUp() {
-        //Debug.Log("Clearing PlayerPrefs");
-        SceneManager.LoadScene("game_DontWakeUp");
+    public void DontWakeUp()
+    {
+        WidgetPopupWindow.Close();
+        GameManager.Instance.Modules.SceneModule.LoadSceneWithTransition("game_DontWakeUp");
         SRDebug.Instance.HideDebugPanel();
     }
 
     [Category("Open Scene")]
-    public void Balloons() {
-        //Debug.Log("Clearing PlayerPrefs");
-        SceneManager.LoadScene("game_Balloons");
+    public void Balloons()
+    {
+        WidgetPopupWindow.Close();
+        GameManager.Instance.Modules.SceneModule.LoadSceneWithTransition("game_Balloons");
         SRDebug.Instance.HideDebugPanel();
     }
 
     [Category("Quality")]
-    public void ToggleQuality() {
-        //Debug.Log("Clearing PlayerPrefs");
+    public void ToggleQuality()
+    {
         AppManager.Instance.ToggleQualitygfx();
         SRDebug.Instance.HideDebugPanel();
     }
