de7cd4053d34430a005f9add5d43994190e20c07
Stefano Cecere <stefano.cecere@krur.com>
2016-07-30 23:09:01 +0200

    [Home] Antura barks

---
+++ Assets/_app/_scripts/Controllers/Dog/AnturaPosing.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using EA4S;
+
+namespace EA4S
+{
+    public class AnturaPosing : MonoBehaviour
+    {
+        	
+        void OnMouseDown()
+        {
+            AudioManager.I.PlaySfx(Sfx.DogBarking);
+        }
+    }
+}
