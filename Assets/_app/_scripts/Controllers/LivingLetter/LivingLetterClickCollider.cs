abeb240a26a979262bf58851a1be27972140dd40
Stefano Cecere <stefano.cecere@krur.com>
2016-07-31 21:11:10 +0200

    [home] the perfect pair for Antura is a Living Letter

---
+++ Assets/_app/_scripts/Controllers/LivingLetter/LivingLetterClickCollider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using EA4S;
+
+namespace EA4S
+{
+    public class LivingLetterClickCollider : MonoBehaviour
+    {
+        public LivingLetter Controller;
+
+        void OnMouseDown()
+        {
+            Controller.Clicked();
+        }
+    }
+}
