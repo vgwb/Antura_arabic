db080f2ddf0737843d1727e7073330ff8f89cc6e
Brago <bragolab@gmail.com>
2016-08-01 02:59:26 +0200

    Fix: break lifecycle app, unable to access to assessment scene.

--- Assets/_app/_scripts/Controllers/Rewards/RewardsManager.cs
+++ Assets/_app/_scripts/Controllers/Rewards/RewardsManager.cs
@@ -13,14 +13,18 @@ namespace EA4S
         {
             AudioManager.I.PlayMusic(Music.Theme4);
 
-            // here we set the Rewards base on current progression level
-            if (AppManager.Instance.PlaySession == 1) {
+            // here we set the Rewards base on current progression level (playsession -1 because Rewards appear when playsession is already incremented)
+            if ((AppManager.Instance.PlaySession -1) == 1) {
                 AnturaController.SetPreset(1);
-            } else if (AppManager.Instance.PlaySession == 2) {
+                LoggerEA4S.Log("app", "Reward", "get_reward", "1");
+            } else if ((AppManager.Instance.PlaySession - 1) == 2) {
                 AnturaController.SetPreset(2);
-            } else if (AppManager.Instance.PlaySession > 2) {
+                LoggerEA4S.Log("app", "Reward", "get_reward", "2");
+            } else if ((AppManager.Instance.PlaySession - 1) > 2) {
                 AnturaController.SetPreset(3);
+                LoggerEA4S.Log("app", "Reward", "get_reward", "3");
             }
+            LoggerEA4S.Save();
 
 
             // Wait for animation to complete
@@ -34,7 +38,7 @@ namespace EA4S
         public void Continue()
         {
             // if we just did Assestment then go mood
-            if (AppManager.Instance.PlaySession > 2) {
+            if ((AppManager.Instance.PlaySession - 1) > 2) {
                 GameManager.Instance.Modules.SceneModule.LoadSceneWithTransition("app_Mood");
             } else {
                 GameManager.Instance.Modules.SceneModule.LoadSceneWithTransition("app_Journey");
