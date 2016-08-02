ba7e5619538e3451cb759ef23b365dab3c96fc26
Stefano Cecere <stefano.cecere@krur.com>
2016-08-03 01:28:46 +0200

    [end] init end manager

---
+++ Assets/_app/_scripts/Controllers/Home/EndManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using EA4S;
+using ModularFramework.Core;
+
+namespace EA4S
+{
+    public class EndManager : MonoBehaviour
+    {
+
+        [Header("Scene Setup")]
+        public Music SceneMusic;
+
+        void Start()
+        {
+          
+
+            AudioManager.I.PlayMusic(SceneMusic);
+            AudioManager.I.PlaySfx(Sfx.GameTitle);
+            ContinueScreen.Show(Play, ContinueScreenMode.Button);
+        }
+
+        public void Play()
+        {
+            GameManager.Instance.Modules.SceneModule.LoadSceneWithTransition("app_Mood");
+        }
+
+
+
+    }
+}
