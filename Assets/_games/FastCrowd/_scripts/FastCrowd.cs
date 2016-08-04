096a385fbcff8034ae32028901c3825a55560617
Brago <bragolab@gmail.com>
2016-08-04 18:23:14 +0200

    FastCrowd: removed wrong sentence on pupup.

--- Assets/_games/FastCrowd/_scripts/FastCrowd.cs
+++ Assets/_games/FastCrowd/_scripts/FastCrowd.cs
@@ -163,7 +163,7 @@ namespace EA4S.FastCrowd {
                 //row.GetStringData("English");
                 PopupMission.Show(new PopupMissionComponent.Data() {
                     // Find the word
-                    Title = string.Format("{0} {1}!", ArabicFixer.Fix(rowLetters.GetStringData("Arabic"), false, false), CompletedWords.Count + 1),
+                    Title = string.Format("{1}", ArabicFixer.Fix(rowLetters.GetStringData("Arabic"), false, false), CompletedWords.Count + 1),
                     MainTextToDisplay = string.Format("{0}", ArabicAlphabetHelper.ParseWord(ActualWord.Word, AppManager.Instance.Letters), sepLetters),
                     Type = PopupMissionComponent.PopupType.New_Mission,
                     DrawSprite = ActualWord.DrawForLivingLetter,
@@ -175,7 +175,7 @@ namespace EA4S.FastCrowd {
                 LocalizationDataRow rowWords = LocalizationData.Instance.GetRow("game_fastcrowd_findwordgroup");
                 PopupMission.Show(new PopupMissionComponent.Data() {
                     // Find the word group
-                    Title = string.Format("{0} {1}!", ArabicFixer.Fix(rowWords.GetStringData("Arabic"), false, false), CompletedWords.Count + 1),
+                    Title = string.Format("{1}", ArabicFixer.Fix(rowWords.GetStringData("Arabic"), false, false), CompletedWords.Count + 1),
                     MainTextToDisplay = string.Format("{0}", stringListOfWords),
                     Type = PopupMissionComponent.PopupType.New_Mission,
                 });
