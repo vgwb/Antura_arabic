098c51dd5bfc6b0ecaec7142c6d54b29b5271427
Stefano Cecere <stefano.cecere@krur.com>
2016-08-04 18:58:49 +0200

    [DontWakeAntura] danger zones on arms and alarms

---
+++ Assets/_games/DontWakeUp/_scripts/Objects/AlertZone.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EA4S
+{
+    public class AlertZone : MonoBehaviour
+    {
+
+        public GameObject DangerLine;
+
+        void Start()
+        {
+            HideDangerLine();
+        }
+
+        void OnEnable()
+        {
+            HideDangerLine();
+        }
+
+        void OnDisable()
+        {
+            HideDangerLine();
+        }
+
+        public void HideDangerLine()
+        {
+            DangerLine.SetActive(false);
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player") {
+                DangerLine.SetActive(true);
+            }
+
+        }
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag == "Player") {
+                DangerLine.SetActive(false);
+            }
+
+        }
+	
+    }
+}
