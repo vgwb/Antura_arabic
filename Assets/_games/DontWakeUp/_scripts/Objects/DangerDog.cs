79fcac62ce346cf016b23da166c8af0daec099b0
Stefano Cecere <stefano.cecere@krur.com>
2016-08-05 00:12:47 +0200

    [DontW] dog danger zone is shown as a whole block, not separated elements

---
+++ Assets/_games/DontWakeUp/_scripts/Objects/DangerDog.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EA4S.DontWakeUp
+{
+    public class DangerDog : MonoBehaviour
+    {
+        public static DangerDog I;
+        public GameObject[] DangerZones;
+
+        void Awake()
+        {
+            I = this;
+            Hide();
+        }
+
+        public void Show()
+        {
+            foreach (var go in DangerZones) {
+                go.SetActive(true);
+            }
+                
+        }
+
+        public void Hide()
+        {
+            foreach (var go in DangerZones) {
+                go.SetActive(false);
+            }
+        }
+    }
+}
