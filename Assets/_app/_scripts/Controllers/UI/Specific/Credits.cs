cce34f9390edfd1eda1015c5526492febaf3319a
Stefano Cecere <stefano.cecere@krur.com>
2016-08-04 08:49:53 +0200

    [home] really basic credits open/close

---
+++ Assets/_app/_scripts/Controllers/UI/Specific/Credits.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EA4S
+{
+    public class Credits : MonoBehaviour
+    {
+
+        public void Open()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Close()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
