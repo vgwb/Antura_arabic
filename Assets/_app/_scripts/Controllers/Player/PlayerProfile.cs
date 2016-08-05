f6998f71f73e81e1871dd5e12611d9773e7b2f45
Stefano Cecere <stefano.cecere@krur.com>
2016-08-05 12:39:46 +0200

    [wheel] show the current won Antura at the Rewards

---
+++ Assets/_app/_scripts/Controllers/Player/PlayerProfile.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EA4S
+{
+    public class PlayerProfile
+    {
+        public int AnturaCurrentPreset;
+
+        public PlayerProfile() {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            AnturaCurrentPreset = 0;
+        }
+    }
+}
