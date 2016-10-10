23f5428fc357b366b055f10421f5e7aa0aadc6a9
Hussam Kazah <nu.urth@gmail.com>
2016-10-10 18:19:33 +0300

    [Dancing Dots] - Rounds - Failures - End of round - End of game

--- Assets/_games/DancingDots/_scripts/DancingDotsGameManager.cs
+++ Assets/_games/DancingDots/_scripts/DancingDotsGameManager.cs
@@ -20,6 +20,9 @@ namespace EA4S.DancingDots
 
 		public DancingDotsGamePlayInfo GameplayInfo;
 
+		public Canvas endGameCanvas;
+
+		public StarFlowers starFlowers;
 
 		public Animator dotsMenu;
 		public Animator diacriticMenu;
@@ -46,10 +49,16 @@ namespace EA4S.DancingDots
 			}
 		}
 
+		public GameObject splatPrefab;
 
 		[Range(0, 255)] public byte dotHintAlpha = 60;
 		[Range(0, 255)] public byte diacriticHintAlpha = 60;
 
+		[Range(0,1)] public float pedagogicalLevel = 0;
+
+		public int numberOfRounds = 6;
+
+		public int allowedFailedMoves = 3;
 
 		public DancingDotsDraggableDot[] dragableDots;
 		public DancingDotsDraggableDot[] dragableDiacritics;
@@ -57,12 +66,15 @@ namespace EA4S.DancingDots
 		private bool isCorrectDot = false;
 		private bool isCorrectDiacritic = false;
 
+		private int numberOfRoundsWon = 0;
+		private int numberOfRoundsPlayed = 0;
+		private int numberOfFailedMoves = 0;
+
+
 		protected override void Awake()
 		{
 			base.Awake();
 			instance = this;
-
-
 		}
 
 		protected override void Start()
@@ -75,12 +87,17 @@ namespace EA4S.DancingDots
 			AppManager.Instance.CurrentGameManagerGO = gameObject;
 			SceneTransitioner.Close();
 
+			StartRound();
 //			StartCoroutine(ShowMenu(dotsMenu));
 
 		}
 
 		public void StartRound()
 		{
+			numberOfRoundsPlayed++;
+			Debug.Log(numberOfRoundsPlayed);
+			numberOfFailedMoves = 0;
+
 			foreach (DancingDotsDraggableDot dDots in dragableDots) dDots.Reset();
 
 			foreach (DancingDotsDraggableDot dDiacritic in dragableDiacritics) dDiacritic.Reset();
@@ -94,23 +111,18 @@ namespace EA4S.DancingDots
 			RandomDiacritic();
 		}
 
-		IEnumerator ShowMenu(Animator animator)
-		{
-			yield return new WaitForSeconds(0.5f);
-			animator.SetTrigger("Show");
-
-
-//			yield return new WaitForSeconds(3f);
-//			StartCoroutine(SwitchMenus(dotsMenu, diacriticMenu));
-
-		}
-
-		IEnumerator SwitchMenus(Animator menu1, Animator menu2)
-		{
-			yield return new WaitForSeconds(0.5f);
-			menu1.SetTrigger("Hide");
-			menu2.SetTrigger("Show");
-		}
+//		IEnumerator ShowMenu(Animator animator)
+//		{
+//			yield return new WaitForSeconds(0.5f);
+//			animator.SetTrigger("Show");
+//		}
+//
+//		IEnumerator SwitchMenus(Animator menu1, Animator menu2)
+//		{
+//			yield return new WaitForSeconds(0.5f);
+//			menu1.SetTrigger("Hide");
+//			menu2.SetTrigger("Show");
+//		}
 
 		protected override void ReadyForGameplay()
 		{
@@ -137,7 +149,6 @@ namespace EA4S.DancingDots
 				go.GetComponent<TextMeshPro>().color = new Color32(currentColor.r,currentColor.g,currentColor.b,0);
 			}
 
-
 			int random = UnityEngine.Random.Range(0, diacritics.Length);
 			activeDiacritic = diacritics[random].GetComponent<DancingDotsDiacriticPosition>();
 		
@@ -151,6 +162,8 @@ namespace EA4S.DancingDots
 			// wait for end of frame to ge correct values for meshes
 			yield return new WaitForEndOfFrame();
 			activeDiacritic.CheckPosition();
+
+			livingLetter.ShowLetters();
 
 			Color32 oldColor = activeDiacritic.GetComponent<TextMeshPro>().color;
 			activeDiacritic.GetComponent<TextMeshPro>().color = new Color32(oldColor.r,oldColor.g,oldColor.b,diacriticHintAlpha);
@@ -172,34 +185,96 @@ namespace EA4S.DancingDots
 		{
 			// Change font or show correct character
 			isCorrectDot = true;
-
 			livingLetter.fullTextGO.SetActive(true);
-
 			StartCoroutine(CorrectMove(isCorrectDiacritic));
 		}
 
 		public void CorrectDiacritic()
 		{
-
 			isCorrectDiacritic = true;
-
 			activeDiacritic.GetComponent<TextMeshPro>().color = new Color32(0,0,0,255);
-
 			StartCoroutine(CorrectMove(isCorrectDot));
+		}
 
+		public void WrongMove(Vector3 pos)
+		{
+			numberOfFailedMoves++;
+			Instantiate(splatPrefab,new Vector3(pos.x,pos.y,-20), Quaternion.identity);
+			if (numberOfFailedMoves >= allowedFailedMoves)
+			{
+				StartCoroutine(RoundLost());
+			}
+
+		}
+
+		IEnumerator CheckNewRound()
+		{
+			if (numberOfRoundsPlayed >= numberOfRounds)
+			{
+				EndGame();
+			}
+			else
+			{
+				livingLetter.GetComponent<Animator>().Play("Pirouette");
+				yield return new WaitForSeconds(0.25f);
+				StartRound();
+				yield return new WaitForSeconds(0.75f);
+				livingLetter.LivingLetterAnimator.Play("walk");
+			}
+		}
+
+		IEnumerator RoundLost()
+		{
+			yield return new WaitForSeconds(0.5f);
+			Debug.Log("Failed");
+			livingLetter.LivingLetterAnimator.Play("ninja");
+			livingLetter.GetComponent<Animator>().Play("FallAndStand");
+			yield return new WaitForSeconds(1.5f);
+
+			StartCoroutine(CheckNewRound());
 		}
 
 		IEnumerator RoundWon()
 		{
+			numberOfRoundsWon++;
 			livingLetter.LivingLetterAnimator.Play("ninja");
 			yield return new WaitForSeconds(0.5f);
-			livingLetter.GetComponent<Animator>().Play("Pirouette");
-			yield return new WaitForSeconds(0.5f);
-			StartRound();
+
+			StartCoroutine(CheckNewRound());
+		}
+
+		private void EndGame()
+		{
+			StartCoroutine(EndGame_Coroutine());
+		}
+
+		private IEnumerator EndGame_Coroutine()
+		{
 			yield return new WaitForSeconds(1f);
-			livingLetter.LivingLetterAnimator.Play("walk");
 
+			endGameCanvas.gameObject.SetActive(true);
 
+			int numberOfStars = 0;
+
+			if (numberOfRoundsWon <= 0) {
+				numberOfStars = 0;
+				WidgetSubtitles.I.DisplaySentence("game_result_retry");
+			} else if ((float)numberOfRoundsWon / numberOfRounds < 0.5f) {
+				numberOfStars = 1;
+				WidgetSubtitles.I.DisplaySentence("game_result_fair");
+			} else if (numberOfRoundsWon < numberOfRounds) {
+				numberOfStars = 2;
+				WidgetSubtitles.I.DisplaySentence("game_result_good");
+			} else {
+				numberOfStars = 3;
+				WidgetSubtitles.I.DisplaySentence("game_result_great");
+			}
+
+			LoggerEA4S.Log("minigame", "DancingDots", "correctLetters", numberOfRoundsWon.ToString());
+			LoggerEA4S.Log("minigame", "DancingDots", "wrongLetters", (numberOfRounds - numberOfRoundsWon).ToString());
+			LoggerEA4S.Save();
+
+			starFlowers.Show(numberOfStars);
 		}
 
 	}
