47bc9b3c40fddb424dcd9d30f2bfb76047e6d011
Fouad Tabsh <tabshf@gmail.com>
2016-10-11 00:03:37 +0300

    [ThrowBalls] Crate and letter physics updates.

---
+++ Assets/_games/ThrowBalls/_scripts/LetterColliderController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EA4S.ThrowBalls
+{
+    public class LetterColliderController : MonoBehaviour
+    {
+        public LetterController letterController;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnCollisionEnter(Collision collision)
+        {
+            letterController.OnCollisionEnter(collision);
+        }
+    }
+}
