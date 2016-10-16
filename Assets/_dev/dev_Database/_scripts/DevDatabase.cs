bdaa82aca8396096cf9665469577df8ec781a6fd
Stefano Cecere <stefano.cecere@krur.com>
2016-10-16 16:34:04 +0200

    [dataset] prepared dev_Database

---
+++ Assets/_dev/dev_Database/_scripts/DevDatabase.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DevDatabase : MonoBehaviour
+{
+
+    public TextAsset MiniGameData;
+    public TextAsset LetterData;
+    public TextAsset WordData;
+    public TextAsset PlaySessionData;
+    public TextAsset AssessmentData;
+    public TextAsset LocalizationData;
+    public TextAsset PhraseData;
+    public TextAsset StageData;
+    public TextAsset RewardData;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
