f7d15a74c1b5db1b15bc1087c894b2200d0042f2
Stefano Cecere <stefano.cecere@krur.com>
2016-10-16 17:04:34 +0200

    [Editor] version window and move Editor scripts into _Scripts

---
+++ Assets/_app/_scripts/Editor/InfoWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace EA4S
+{
+
+    public class InfoView : EditorWindow
+    {
+        [MenuItem("Tools/EA4S Antura")]
+        static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(EA4S.InfoView));
+        }
+
+        void OnGUI()
+        {
+            this.titleContent.text = "EA4S Antura";
+            EditorGUILayout.LabelField("Hi there! I'm Antura and i'm version " + AppManager.AppVersion);
+        }
+
+    }
+}
