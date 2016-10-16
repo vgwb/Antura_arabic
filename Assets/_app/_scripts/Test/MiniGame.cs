2942abbdca222904a683790404f0208204c4aad5
Elena Blanes <elenita_bf@hotmail.com>
2016-10-16 19:08:19 +0200

    [feature] Add basic script IA for Game Selector (under Test)

---
+++ Assets/_app/_scripts/Test/MiniGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MiniGame : MonoBehaviour {
+
+    public string nameMiniGame;
+    public float height;
+    public bool forced;
+    public int order;
+    public int playSession;
+	
+    public MiniGame(string nameM, float heightM, bool forcedM, int orderM)
+    {
+        nameMiniGame = nameM;
+        height = heightM;
+        forced = forcedM;
+        order = orderM;
+    }
+}
