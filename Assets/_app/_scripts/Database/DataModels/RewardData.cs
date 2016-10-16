f2fa88b73e90c71d6dc6179d9d92f365e35dc9e9
Stefano Cecere <stefano.cecere@krur.com>
2016-10-16 15:41:35 +0200

    [data] RewardData

---
+++ Assets/_app/_scripts/Database/DataModels/RewardData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace EA4S.Db
+{
+    [Serializable]
+    public class RewardData
+    {
+        public string Id;
+        public string Title;
+        public RewardCategory Category;
+    }
+
+    public enum RewardCategory
+    {
+        DogProp,
+        DogSkin,
+        PlayerTitle
+    }
+
+}
