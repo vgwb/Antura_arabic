0d915a4670cc7423abc8d869101753cb2e13cf97
Stefano Cecere <stefano.cecere@krur.com>
2016-10-18 23:30:47 +0200

    [data] added Method to ArabicHelper to prepare arabic string for TMPro (and improved DB manager scene)

--- Assets/_manage/manage_Database/_scripts/Editor/DatabaseLoaderInspector.cs
+++ Assets/_manage/manage_Database/_scripts/Editor/DatabaseLoaderInspector.cs
@@ -4,38 +4,38 @@ using UnityEngine.UI;
 
 namespace EA4S.Db.Management.Editor
 {
-    [CustomEditor(typeof(DatabaseLoader))]
-    public class DatabaseLoaderInspector : UnityEditor.Editor
-    {
-        DatabaseLoader src;
+    //[CustomEditor(typeof(DatabaseLoader))]
+    //public class DatabaseLoaderInspector : UnityEditor.Editor
+    //{
+    //    DatabaseLoader src;
 
-        void OnEnable()
-        {
-            src = target as DatabaseLoader;
-        }
+    //    void OnEnable()
+    //    {
+    //        src = target as DatabaseLoader;
+    //    }
 
-        public override void OnInspectorGUI()
-        {
-            DrawDefaultInspector();
+    //    public override void OnInspectorGUI()
+    //    {
+    //        DrawDefaultInspector();
 
-            if (GUILayout.Button("Load All Data"))
-            {
-                if (AllDataIsSet())
-                {
-                    src.LoadDatabase();
-                }
-                else
-                {
-                    Debug.LogError("Error: not all references to data files were correctly added to the DatabaseLoader inspector.");
-                }
-            }
-        }
+    //        if (GUILayout.Button("Load All Data"))
+    //        {
+    //            if (AllDataIsSet())
+    //            {
+    //                src.LoadDatabase();
+    //            }
+    //            else
+    //            {
+    //                Debug.LogError("Error: not all references to data files were correctly added to the DatabaseLoader inspector.");
+    //            }
+    //        }
+    //    }
 
-        private bool AllDataIsSet()
-        {
-            if (!src.database) return false;
-            return src.inputData.AllDataIsSet();
-        }
+    //    private bool AllDataIsSet()
+    //    {
+    //        if (!src.database) return false;
+    //        return src.inputData.AllDataIsSet();
+    //    }
 
-    }
+    //}
 }
