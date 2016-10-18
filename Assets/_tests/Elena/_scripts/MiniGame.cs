a0b6d924346f1700dfe0b8fcf49b7548c9da1983
Elena Blanes <elenita_bf@hotmail.com>
2016-10-18 10:28:00 +0200

    [changed] wrong name variable weight in IAWheels and MiniGame

--- Assets/_tests/Elena/_scripts/MiniGame.cs
+++ Assets/_tests/Elena/_scripts/MiniGame.cs
@@ -5,15 +5,15 @@ using System.Collections;
 public class MiniGame : MonoBehaviour {
 
     public string nameMiniGame;
-    public float height;
+    public float weight;
     public bool forced;
     public int order;
     public int playSession;
 	
-    public MiniGame(string nameM, float heightM, bool forcedM, int orderM)
+    public MiniGame(string nameM, float weightM, bool forcedM, int orderM)
     {
         nameMiniGame = nameM;
-        height = heightM;
+        weight = weightM;
         forced = forcedM;
         order = orderM;
     }
