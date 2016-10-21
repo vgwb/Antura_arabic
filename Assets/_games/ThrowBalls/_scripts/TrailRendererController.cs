a13bfa318c9043762ec9904e5efb16a7d2e1ab53
Fouad Tabsh <tabshf@gmail.com>
2016-10-21 12:53:06 +0300

    [ThrowBalls] Last commit before trying out slingshot.

---
+++ Assets/_games/ThrowBalls/_scripts/TrailRendererController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EA4S.ThrowBalls
+{
+    public class TrailRendererController : MonoBehaviour
+    {
+        public TrailRenderer trailRenderer;
+        private bool isFollowingPokeball = false;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isFollowingPokeball)
+            {
+                transform.position = PokeballController.instance.transform.position;
+            }
+        }
+
+        public void Reset()
+        {
+            trailRenderer.Clear();
+        }
+
+        public void SetIsFollowPokeball(bool isFollowingPokeball)
+        {
+            this.isFollowingPokeball = isFollowingPokeball;
+        }
+    }
+}
