f9b12427a62733128831d87580fef698800f3bf1
Davide Barbieri <davide.barbieri@ghostshark.it>
2016-10-24 00:17:37 +0200

    [FastCrowd] Letters walk in center of the screen [FastCrowd] Max number of letters on screen based on difficulty [FastCrowd] Letters turn faster

--- Assets/_games/FastCrowd/_scripts/NewVersion/LettersStates/LetterHangingState.cs
+++ Assets/_games/FastCrowd/_scripts/NewVersion/LettersStates/LetterHangingState.cs
@@ -35,7 +35,7 @@ namespace EA4S.FastCrowd
                 near[i].Scare(letter.transform.position, 2);
 
             // Face Camera!
-            movement.LerpLookAt(Camera.main.transform.position, 3*delta);
+            movement.LerpLookAt(Camera.main.transform.position, 8*delta);
         }
 
         public override void UpdatePhysics(float delta)
