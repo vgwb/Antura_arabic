e6032166d0bb5c01ec220cf15bf0677bc450eedd
Fouad Tabsh <tabshf@gmail.com>
2016-10-29 16:29:09 +0300

    [ThrowBalls] Add fake shadow for ball.

---
+++ Assets/_games/ThrowBalls/_scripts/BallShadowController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EA4S.ThrowBalls
+{
+    public class BallShadowController : MonoBehaviour
+    {
+        public static BallShadowController instance;
+
+        void Awake()
+        {
+            instance = this;
+        }
+
+        void Update()
+        {
+            Vector3 ballPosition = BallController.instance.transform.position;
+            Vector3 groundPosition = GroundController.instance.transform.position;
+
+            Vector3 shadowPosition = ballPosition;
+            shadowPosition.y = ballPosition.y > groundPosition.y ? groundPosition.y + 0.01f : ballPosition.y;
+            transform.position = shadowPosition;
+
+            float ballElevation = ballPosition.y - groundPosition.y;
+
+            float scale = 0.4f + ballElevation * 0.1f / 7f;
+            transform.localScale = new Vector3(scale, scale, 1);
+        }
+
+        public void Enable()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Disable()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
