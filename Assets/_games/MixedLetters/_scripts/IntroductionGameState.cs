d30a3d969e9924065b8149cebf4feb99ac2acc66
Fouad Tabsh <tabshf@gmail.com>
2016-10-31 17:01:06 +0200

    [MixedLetters] Major updates.

--- Assets/_games/MixedLetters/_scripts/IntroductionGameState.cs
+++ Assets/_games/MixedLetters/_scripts/IntroductionGameState.cs
@@ -6,7 +6,11 @@ namespace EA4S.MixedLetters
     {
         MixedLettersGame game;
 
-        float timer = 4;
+        private float anturaEnterTimer;
+        private bool anturaEntered = false;
+        private float anturaExitTimer;
+        private bool anturaExited = false;
+
         public IntroductionGameState(MixedLettersGame game)
         {
             this.game = game;
@@ -14,10 +18,15 @@ namespace EA4S.MixedLetters
 
         public void EnterState()
         {
-            timer = 4;
+            anturaEnterTimer = Random.Range(1f, 2f);
+            anturaEntered = false;
+            anturaExitTimer = Random.Range(0.75f, 1.5f);
+            anturaExited = false;
             game.GenerateNewWord();
-
-            SeparateLettersSpawnerController.instance.SpawnLetters(game.lettersInOrder);
+            VictimLLController.instance.Enable();
+            Vector3 victimLLPosition = VictimLLController.instance.transform.position;
+            victimLLPosition.x = Random.Range(0, 40) % 2 == 0 ? 0.5f : -0.5f;
+            VictimLLController.instance.SetPosition(victimLLPosition);
         }
 
         public void ExitState()
@@ -26,16 +35,46 @@ namespace EA4S.MixedLetters
 
         public void Update(float delta)
         {
-            timer -= delta;
-
-            if (timer < 0)
+            anturaEnterTimer -= delta;
+            if (anturaEnterTimer < 0 && !anturaEntered)
             {
-                game.SetCurrentState(game.PlayState);
+                AnturaController.instance.Enable();
+                AnturaController.instance.EnterScene(OnFightBegan);
+                anturaEntered = true;
+            }
+
+            if (anturaExited)
+            {
+                anturaExitTimer -= delta;
+
+                if (anturaExitTimer < 0)
+                {
+                    game.SetCurrentState(game.PlayState);
+                }
             }
         }
 
         public void UpdatePhysics(float delta)
         {
+        }
+
+        public void OnFightBegan()
+        {
+            AnturaController.instance.SetPosition(VictimLLController.instance.transform.position);
+            AnturaController.instance.Disable();
+            VictimLLController.instance.Disable();
+            ParticleSystemController.instance.Enable();
+            ParticleSystemController.instance.SetPosition(VictimLLController.instance.transform.position);
+            SeparateLettersSpawnerController.instance.SetPosition(VictimLLController.instance.transform.position);
+            SeparateLettersSpawnerController.instance.SpawnLetters(game.lettersInOrder, OnFightEnded);
+        }
+
+        public void OnFightEnded()
+        {
+            AnturaController.instance.Enable();
+            AnturaController.instance.SetPositionWithOffset(VictimLLController.instance.transform.position, new Vector3(0, 0, 1f));
+            ParticleSystemController.instance.Disable();
+            anturaExited = true;
         }
     }
 }
--- Assets/_games/MixedLetters/_scripts/SeparateLettersSpawnerController.cs
+++ Assets/_games/MixedLetters/_scripts/SeparateLettersSpawnerController.cs
@@ -12,6 +12,8 @@ namespace EA4S.MixedLetters
 
         private IEnumerator spawnLettersCoroutine;
 
+        public AudioSource audioSource;
+
         void Awake()
         {
             instance = this;
@@ -29,19 +31,30 @@ namespace EA4S.MixedLetters
 
         }
 
-        public void SpawnLetters(List<LL_LetterData> lettersToSpawn)
+        public void SetPosition(Vector3 position)
         {
-            spawnLettersCoroutine = SpawnLettersCoroutine(lettersToSpawn);
+            transform.position = position;
+        }
+
+        public void SpawnLetters(List<LL_LetterData> lettersToSpawn, System.Action spawnOverCallback)
+        {
+            spawnLettersCoroutine = SpawnLettersCoroutine(lettersToSpawn, spawnOverCallback);
             StartCoroutine(spawnLettersCoroutine);
         }
 
-        private IEnumerator SpawnLettersCoroutine(List<LL_LetterData> lettersToSpawn)
+        private IEnumerator SpawnLettersCoroutine(List<LL_LetterData> lettersToSpawn, System.Action spawnOverCallback)
         {
+            audioSource.pitch = Random.Range(0.8f, 1.2f);
+            audioSource.Play();
+            yield return new WaitForSeconds(1);
+
             List<int> indices = new List<int>();
             for (int i = 0; i < lettersToSpawn.Count; i++)
             {
                 indices.Add(i);
             }
+
+            bool throwLetterToTheRight = Random.Range(1, 40) % 2 == 0;
 
             for (int i = 0; i < lettersToSpawn.Count; i++)
             {
@@ -52,12 +65,23 @@ namespace EA4S.MixedLetters
 
                 SeparateLetterController separateLetterController = separateLetterControllers[i];
                 separateLetterController.Enable();
-                separateLetterController.SetPosition(transform.position);
+                separateLetterController.SetPosition(transform.position, false);
                 separateLetterController.SetLetter(letterToSpawn);
                 separateLetterController.SetRotation(new Vector3(0, 0, Random.Range(0, 4) * 90));
                 separateLetterController.SetIsKinematic(false);
-                yield return new WaitForSeconds(0.8f);
+                separateLetterController.AddForce(new Vector3(throwLetterToTheRight ? Random.Range(2f, 6f) : Random.Range(-6f, -2f), Constants.GRAVITY.y * -0.45f), ForceMode.VelocityChange);
+
+                throwLetterToTheRight = !throwLetterToTheRight;
+
+                MixedLettersConfiguration.Instance.Context.GetAudioManager().PlaySound(Sfx.ThrowObj);
+
+                yield return new WaitForSeconds(0.75f);
             }
+
+            yield return new WaitForSeconds(1);
+            audioSource.Stop();
+
+            spawnOverCallback.Invoke();
         }
 
         public void SetLettersDraggable(bool isDraggable)
