a95cc822861c7fb3cdece5f26aef782b1f8fbe15
Demigiant <daniele.giardini@gmail.com>
2016-11-01 18:28:23 +0100

    [TutorialUI] Preparing framework and test scene + [GamesSelector] Implemented "AutoLoadMinigamesOnStartup" option

---
+++ Assets/_app/_scripts/Controllers/Tutorial/TutorialUISystem/Tester_TutorialUI.cs
@@ -0,0 +1,48 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2016/11/01
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EA4S
+{
+    public class Tester_TutorialUI : MonoBehaviour
+    {
+        public float CameraDistance = 20;
+        public float MinPointDistance = 0.3f;
+
+        readonly List<Vector3> storedPs = new List<Vector3>();
+        bool isDragging;
+
+        #region Unity
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0)) {
+                isDragging = true;
+                storedPs.Add(MouseWorldPosition());
+            } else if (isDragging) {
+                Vector3 p = MouseWorldPosition();
+                if (Vector3.Distance(storedPs[storedPs.Count - 1], p) >= MinPointDistance) storedPs.Add(p);
+            }
+
+            if (Input.GetMouseButtonUp(0)) {
+                isDragging = false;
+                storedPs.Add(MouseWorldPosition());
+                TutorialUI.DrawLine(storedPs.ToArray(), false, false);
+                storedPs.Clear();
+            }
+        }
+
+        #endregion
+
+        #region Helpers
+
+        Vector3 MouseWorldPosition()
+        {
+            return Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane + CameraDistance));
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/_app/_scripts/Controllers/Tutorial/TutorialUISystem/TutorialUIPools.cs
@@ -0,0 +1,39 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2016/11/01
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EA4S
+{
+    public class TutorialUIPools : MonoBehaviour
+    {
+        public TrailRenderer TrailPrefab;
+
+        [System.NonSerialized] public TutorialUI TutorialUI;
+        readonly List<TrailRenderer> trailsPool = new List<TrailRenderer>();
+
+        #region Public Methods
+
+        public TrailRenderer SpawnTrail(Vector3 _position)
+        {
+            foreach (TrailRenderer tr in trailsPool) {
+                if (tr.gameObject.activeSelf) continue;
+                tr.transform.position = _position;
+                tr.Clear();
+                return tr;
+            }
+            TrailRenderer newTrail = Instantiate(TrailPrefab, _position, Quaternion.identity, TrailPrefab.transform.parent) as TrailRenderer;
+            trailsPool.Add(newTrail);
+
+            // Adapt size to camera
+            // 1 : 45 = x : fov
+            newTrail.startWidth = Camera.main.fieldOfView * TutorialUI.TrailSize / 45f;
+            newTrail.endWidth = newTrail.startWidth * 0.4f;
+
+            return newTrail;
+        }
+
+        #endregion
+    }
+}
