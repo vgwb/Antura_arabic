438823701eb3597e2335144f360a6020fe4fdcfa
Fouad Tabsh <tabshf@gmail.com>
2016-11-03 15:42:33 +0200

    [MixedLetters] Limit rounds to 6 and show end game scenario (stars).

--- Assets/_games/MixedLetters/_scripts/ResultGameState.cs
+++ Assets/_games/MixedLetters/_scripts/ResultGameState.cs
@@ -5,6 +5,9 @@
         IPopupWidget popupWidget;
         MixedLettersGame game;
 
+        float endResultTimer = 1f;
+        bool isGameOver = false;
+
         public ResultGameState(MixedLettersGame game)
         {
             this.game = game;
@@ -31,12 +34,48 @@
         public void OnResultPressed()
         {
             popupWidget.Hide();
-            game.SetCurrentState(game.IntroductionState);
+
+            if (game.roundNumber < 6)
+            {
+                game.SetCurrentState(game.IntroductionState);
+            }
+            
+            else
+            {
+                isGameOver = true;
+            }
         }
 
         public void Update(float delta)
         {
+            if (isGameOver)
+            {
+                endResultTimer -= delta;
 
+                if (endResultTimer < 0)
+                {
+                    int numberOfStars;
+
+                    if (game.numRoundsWon <= 0)
+                    {
+                        numberOfStars = 0;
+                    }
+                    else if (game.numRoundsWon <= 2)
+                    {
+                        numberOfStars = 1;
+                    }
+                    else if (game.numRoundsWon <= 5)
+                    {
+                        numberOfStars = 2;
+                    }
+                    else
+                    {
+                        numberOfStars = 3;
+                    }
+
+                    game.EndGame(numberOfStars, 0);
+                }
+            }
         }
 
         public void UpdatePhysics(float delta)
