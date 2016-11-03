1a718887e5db053d2d1f907dd017f54086e47cd5
Joseph Saade <Joseph Saade>
2016-11-03 02:19:23 +0200

    Use correct letters and respective tubes Refactor sates MAke the letter go up and down tubes Make the letter re-apear from tube if dropped or put in wrong tube Make each round start from the right Every 10 seconds Antura will chase letter off and LL will re-drop from tube

--- Assets/_games/TakeMeHome/_scripts/TakeMeHomeGame.cs
+++ Assets/_games/TakeMeHome/_scripts/TakeMeHomeGame.cs
@@ -12,7 +12,16 @@ namespace EA4S.TakeMeHome
 		public TextMeshProUGUI roundText;
 		public GameObject tubes;
 		public GameObject spawnTube;
-		
+		public Transform LLSpawnPosition;
+		public GameObject antura;
+
+		[HideInInspector]
+		public TakeMeHomeLL currentLetter;
+
+		[HideInInspector]
+		public int currentTube = 0;
+
+		private int _activeTubes = 0;
 
 		public int CurrentScore { get; private set; }
 
@@ -29,6 +38,9 @@ namespace EA4S.TakeMeHome
 
 		[HideInInspector]
 		public int currentRound;
+
+		[HideInInspector]
+		public CountdownTimer gameTime;
 
 		int stars1Threshold
 		{
@@ -73,21 +85,27 @@ namespace EA4S.TakeMeHome
 		public TakeMeHomePlayState PlayState { get; private set; }
 		public TakeMeHomeResultState ResultState { get; private set; }
 		public TakeMeHomeEndState EndState { get; private set; }
+		public TakeMeHomeResetState ResetState { get; private set; }
+		public TakeMeHomeAnturaState AntureState { get; private set; }
 
-		public void InitTubes(int count = 2)
+		public void InitTubes()
 		{
 			letterManager = GetComponent<TakeMeHomeLetterManager> ();
 			activeTubes = new List<GameObject> ();
-			int c = 0;
+
 			foreach (Transform child in tubes.transform) {
+				child.gameObject.name = "tube_" + activeTubes.Count;
 				child.gameObject.SetActive (false);
 				activeTubes.Add (child.gameObject);
+
 			}
+
 		}
 
 
 		public void activateTubes(int count = 2)
 		{
+			_activeTubes = count;
 			for (int i = 0; i < count; ++i)
 				activeTubes [i].SetActive (true);
 		}
@@ -105,6 +123,7 @@ namespace EA4S.TakeMeHome
 
 		public void IncrementRound()
 		{
+			currentLetter = null;
 			++currentRound;
 			letterManager.removeLetter ();
 
@@ -123,8 +142,19 @@ namespace EA4S.TakeMeHome
 
 			roundText.text = "#"+currentRound.ToString ();
 
-			letterManager.spawnLetter (TakeMeHomeConfiguration.Instance.Letters.GetNextData ());
+			spawnLetteAtTube ();
+			//letterManager.spawnLetter (AppManager.Instance.Letters [TakeMeHomeModel.Instance.getRandomLetterOnTube(0)]);
 		}
+
+		void spawnLetteAtTube()
+		{
+			
+			currentTube = UnityEngine.Random.Range(0,_activeTubes);
+			currentLetter = letterManager.spawnLetter (AppManager.Instance.Letters [TakeMeHomeModel.Instance.getRandomLetterOnTube(currentTube)]);
+			currentLetter.MoveBy (new UnityEngine.Vector3 (-13, 0, 0),2);
+		}
+
+		 
 
 		protected override IGameConfiguration GetConfiguration()
 		{
@@ -144,6 +174,8 @@ namespace EA4S.TakeMeHome
 			PlayState = new TakeMeHomePlayState(this);
 			ResultState = new TakeMeHomeResultState(this);
 			EndState = new TakeMeHomeEndState(this);
+			ResetState = new TakeMeHomeResetState (this);
+			AntureState = new TakeMeHomeAnturaState (this);
 
 			timerText.gameObject.SetActive(false);
 			roundText.gameObject.SetActive(false);
@@ -152,11 +184,44 @@ namespace EA4S.TakeMeHome
 
 			Physics.gravity = Vector3.up * -40;
 
+			InitTubes ();
 
+
+			//setup timer and round info:
+			currentRound = 0;
+			roundText.text = "#"+currentRound.ToString ();
+
+			Context.GetAudioManager().PlayMusic(Music.Theme3);
+
+
+			gameTime = new CountdownTimer(UnityEngine.Mathf.Lerp(90.0f, 60.0f, TakeMeHomeConfiguration.Instance.Difficulty));
+			gameTime.onTimesUp += OnTimesUp;
+
+			gameTime.Reset();
 
 		}
 
+		public void followLetter()
+		{
+			if (!currentLetter)
+				return;
+
+			currentLetter.panicAndRun ();
+
+			antura.SetActive (true);
+
+			//antura.GetComponent<AnturaCo
+
+			antura.GetComponent<TakeMeHomeAntura> ().SetAnturaTime (true, currentLetter.transform.position - new Vector3(5,0,0));
+		}
 
 
+		void OnTimesUp()
+		{
+			// Time's up!
+			isTimesUp = true;
+
+			this.SetCurrentState(EndState);
+		}
 	}
 }
