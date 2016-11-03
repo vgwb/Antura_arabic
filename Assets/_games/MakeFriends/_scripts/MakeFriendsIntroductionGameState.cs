77328764712ba797b60c6e34b7b35903dca65bc4
George H <george.hbr@gmail.com>
2016-11-03 18:58:44 +0200

    [Make Friends] Updating Core API compliance

---
+++ Assets/_games/MakeFriends/_scripts/MakeFriendsIntroductionGameState.cs
@@ -0,0 +1,35 @@
+﻿namespace EA4S.MakeFriends
+{
+    public class MakeFriendsIntroductionGameState : IGameState
+    {
+        MakeFriendsGame game;
+
+        float timer = 4;
+        public MakeFriendsIntroductionGameState(MakeFriendsGame game)
+        {
+            this.game = game;
+        }
+
+        public void EnterState()
+        {
+        }
+
+        public void ExitState()
+        {
+        }
+
+        public void Update(float delta)
+        {
+            timer -= delta;
+
+            if (timer < 0)
+            {
+                game.SetCurrentState(game.QuestionState);
+            }
+        }
+
+        public void UpdatePhysics(float delta)
+        {
+        }
+    }
+}
