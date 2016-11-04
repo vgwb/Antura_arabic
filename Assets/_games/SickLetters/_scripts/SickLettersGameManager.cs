03e86bee4ee412e339a8154d4432c179c489b91d
ModarD <skullyubel@gmail.com>
2016-11-05 00:00:27 +0200

    [Sick Letters] New game, core gameplay

---
+++ Assets/_games/SickLetters/_scripts/SickLettersGameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EA4S.SickLetters
+{
+    public class SickLettersGameManager : MonoBehaviour
+    {
+
+        public SickLettersGame game;
+        
+
+        
+
+        // Use this for initialization
+        void Start()
+        {
+            game = GetComponent<SickLettersGame>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        
+    }
+}
