b57845f2d4daf8a085f550038b8bb192bb680527
Stefano Cecere <stefano.cecere@krur.com>
2016-11-05 00:47:02 +0100

    removed unused methods

--- Assets/_app/_scripts/Controllers/LivingLetter/Data/LL_WordData.cs
+++ Assets/_app/_scripts/Controllers/LivingLetter/Data/LL_WordData.cs
@@ -26,35 +26,6 @@ namespace EA4S
             Data = _data;
         }
 
-        #region API
-
-        /// <summary>
-        /// Get WordData by keyRow.
-        /// </summary>
-        /// <param name="_keyRow"></param>
-        /// <returns></returns>
-        public static LL_WordData GetWordDataByKeyRow(string _keyRow)
-        {
-            // wordsRow wr = words.Instance.GetRow(_keyRow);
-
-            Db.WordData wdata = AppManager.Instance.DB.GetWordDataById(_keyRow);
-            LL_WordData wd = new LL_WordData(_keyRow, wdata);
-            return wd;
-        }
-
-
-        public static LL_WordData GetRandomWord()
-        {
-            // wordsRow wr = words.Instance.GetRow(_keyRow);
-
-            Db.WordData wdata = AppManager.Instance.DB.GetWordDataByRandom();
-            LL_WordData wd = new LL_WordData(wdata.GetId(), wdata);
-            return wd;
-        }
-
-
-        #region API
-
         /// <summary>
         /// Living Letter Text To Display.
         /// </summary>
@@ -69,8 +40,5 @@ namespace EA4S
             get { return Resources.Load<Sprite>("Textures/LivingLetters/Drawings/drawing-" + Key); }
         }
 
-        #endregion
-
-        #endregion
     }
 }
