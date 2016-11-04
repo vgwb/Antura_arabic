3fd818f8583f65f6e5a0ca92c0cb2779f1aa3d33
Stefano Cecere <stefano.cecere@krur.com>
2016-11-04 09:24:55 +0100

    [Debug] DebugManager to have minigames settings and cheat codes

---
+++ Assets/_app/_scripts/Debug/DebugManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EA4S
+{
+    public enum DifficulyLevels
+    {
+        VeryEasy = 0,
+        Easy = 1,
+        Normal = 2,
+        Hard = 3,
+        VeryHard = 4
+    }
+
+    public class DebugManager : MonoBehaviour
+    {
+        public static DebugManager I;
+
+        DifficulyLevels _DifficultyLevel = DifficulyLevels.Normal;
+        public DifficulyLevels DifficultyLevel {
+            get { return _DifficultyLevel; }
+            set {
+                _DifficultyLevel = value;
+                switch (_DifficultyLevel) {
+                    case DifficulyLevels.VeryEasy:
+                        Difficulty = 0.1f;
+                        break;
+                    case DifficulyLevels.Easy:
+                        Difficulty = 0.3f;
+                        break;
+                    case DifficulyLevels.Normal:
+                        Difficulty = 0.5f;
+                        break;
+                    case DifficulyLevels.Hard:
+                        Difficulty = 0.7f;
+                        break;
+                    case DifficulyLevels.VeryHard:
+                        Difficulty = 1.0f;
+                        break;
+                }
+            }
+        }
+        public float Difficulty = 0.5f;
+        public int Stage = 1;
+        public int LearningBlock = 1;
+
+        void Awake()
+        {
+            I = this;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                Debug.Log("DEBUG - SPACE : skip");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad0) || Input.GetKeyDown(KeyCode.Alpha0)) {
+                Debug.Log("DEBUG - 0");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1)) {
+                Debug.Log("DEBUG - 1");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Alpha2)) {
+                Debug.Log("DEBUG - 2");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.Alpha3)) {
+                Debug.Log("DEBUG - 3");
+            }
+        }
+    }
+}
