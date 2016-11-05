df789522b9d818fd6ba751056d296eb85755d20a
pernigoe <pernigoe@gmail.com>
2016-11-05 11:49:10 +0100

    [MissingLetter] Fix Finger and Light

--- Assets/_games/MissingLetter/_scripts/FingerSuggestion.cs
+++ Assets/_games/MissingLetter/_scripts/FingerSuggestion.cs
@@ -6,13 +6,21 @@ public class FingerSuggestion : MonoBehaviour
 {
     Tween t;
 
-    public void DoSuggestion()
+    public void DoSuggestion(GameObject target)
+    {
+        gameObject.SetActive(true);
+        transform.position = target.transform.position + Vector3.back * 3f + Vector3.up * 3;
+        transform.parent = target.transform;
+        DoSuggMove();
+    }
+
+    private void DoSuggMove()
     {
         t = transform.DOMoveZ(transform.position.z + 2, 0.7f).OnComplete(
-            delegate
-            {
-                t = transform.DOMoveZ(transform.position.z - 2, 0.7f).OnComplete(DoSuggestion);
-            });
+          delegate
+          {
+              t = transform.DOMoveZ(transform.position.z - 2, 0.7f).OnComplete(DoSuggMove);
+          });
     }
 
     void OnDisable()
