6a864d646642eabfb9c246922f0ff200a62f732e
Michele <cat.piro@gmail.com>
2016-11-07 15:34:05 +0100

    Added question builders to Maze, MixedLetters, ThrowBalls (missing code!)

--- Assets/_games/ThrowBalls/_configurationscripts/ThrowBallsConfiguration.cs
+++ Assets/_games/ThrowBalls/_configurationscripts/ThrowBallsConfiguration.cs
@@ -49,7 +49,21 @@
 
         public IQuestionBuilder SetupBuilder() {
             IQuestionBuilder builder = null;
-            // TODO
+
+            int nPacks = 10;
+            int nCorrect = 4;
+            int nWrong = 4;
+
+            switch (Variation)
+            {
+                case ThrowBallsVariation.letters:
+                    builder = new RandomLettersQuestionBuilder(nPacks, nCorrect, nWrong);
+                    break;
+                case ThrowBallsVariation.words:
+                    builder = new RandomWordsQuestionBuilder(nPacks, 1, nWrong, firstCorrectIsQuestion:true);
+                    break;
+            }
+
             return builder;
         }
     }
