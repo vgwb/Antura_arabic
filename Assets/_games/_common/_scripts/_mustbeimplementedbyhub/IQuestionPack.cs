b11d90807135bb50fd002b3b700baa5ac1ec4ac3
Brago <bragolab@gmail.com>
2016-11-08 12:23:28 +0100

    - Added multiple question support to IQuestionPack interface and refactor for any concrete implementation

--- Assets/_games/_common/_scripts/_mustbeimplementedbyhub/IQuestionPack.cs
+++ Assets/_games/_common/_scripts/_mustbeimplementedbyhub/IQuestionPack.cs
@@ -6,9 +6,9 @@ namespace EA4S
     /// A question pack: which includes a question (under the form of a letter, word or image),
     /// and a set of wrong answers and correct andwers of the same format.
     /// </summary>
-   public interface IQuestionPack : IGameData
-    {
+   public interface IQuestionPack : IGameData {
         ILivingLetterData GetQuestion();
+        IEnumerable<ILivingLetterData> GetQuestions();
         IEnumerable<ILivingLetterData> GetWrongAnswers();
         IEnumerable<ILivingLetterData> GetCorrectAnswers();
     }
