a107f0ef6a44936c41ed9036fe260ac655911e91
Davide Barbieri <davide.barbieri@ghostshark.it>
2016-11-08 13:19:02 +0100

    [ReadingGame] Multiple bars

--- Assets/_games/ReadingGame/Scripts/ReadingBar.cs
+++ Assets/_games/ReadingGame/Scripts/ReadingBar.cs
@@ -7,6 +7,7 @@ public class ReadingBar : MonoBehaviour
 
     public RectTransform start;
     public RectTransform end;
+    public RectTransform endCompleted;
 
     [Range(0, 1)]
     public float currentReading = 0;
@@ -15,11 +16,34 @@ public class ReadingBar : MonoBehaviour
 
     public MagnifingGlass glass;
 
+    bool active;
+    public bool Active
+    {
+        get
+        {
+            return active;
+        }
+        set
+        {
+            active = value;
+
+            glass.gameObject.SetActive(active);
+        }
+
+    }
+
+    void Awake()
+    {
+        Active = false;
+    }
+
     void Start()
     {
         Update();
 
         glass.transform.position = GetGlassWorldPosition();
+        end.gameObject.SetActive(true);
+        endCompleted.gameObject.SetActive(false);
     }
 
     void Update ()
@@ -33,11 +57,19 @@ public class ReadingBar : MonoBehaviour
         var oldEndPos = end.localPosition;
         oldEndPos.x = - size.x * 0.5f - endOffset;
         end.localPosition = oldEndPos;
+        endCompleted.localPosition = oldEndPos;
 
         glass.transform.position = Vector3.Lerp(glass.transform.position, GetGlassWorldPosition(), Time.deltaTime*20);
     }
 
-    public void SetGlassScreenPosition(Vector2 position)
+    public void Complete()
+    {
+        end.gameObject.SetActive(false);
+        endCompleted.gameObject.SetActive(true);
+        currentReading = 1;
+    }
+
+    public bool SetGlassScreenPosition(Vector2 position)
     {
         var startScreen = Camera.main.WorldToScreenPoint(start.position);
         var endScreen = Camera.main.WorldToScreenPoint(end.position);
@@ -45,12 +77,16 @@ public class ReadingBar : MonoBehaviour
         var glassScreenSize = Camera.main.WorldToScreenPoint(glass.transform.position + glass.GetSize())
             - Camera.main.WorldToScreenPoint(glass.transform.position);
 
+        bool completed = false;
         if (Mathf.Abs(endScreen.x - position.x) < Mathf.Abs(glassScreenSize.x) / 2)
         {
             position = endScreen;
+            completed = true;
         }
 
         currentReading = 1.0f - Mathf.Clamp01((position.x - endScreen.x) / (startScreen.x - endScreen.x));
+
+        return completed;
     }
 
     public Vector2 GetGlassScreenPosition()
--- Assets/_games/ReadingGame/Scripts/ReadingBarSet.cs
+++ Assets/_games/ReadingGame/Scripts/ReadingBarSet.cs
@@ -1,19 +1,109 @@
 ﻿using System;
 using EA4S;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ReadingBarSet : MonoBehaviour
 {
     public bool active = true;
 
-    public ReadingBar[] bars;
+    public ReadingBar readingBarPrefab;
 
-    public ReadingBar activeBar;
+    List<ReadingBar> bars = new List<ReadingBar>();
+
     public Camera mainCamera;
+
+    public Transform barsStart;
+    public float distanceBetweenBars = 3;
+
+    ReadingBar activeBar;
+
+    void Start()
+    {
+        SetData(null);
+    }
+
+    void SetActiveBar(ReadingBar bar)
+    {
+        if (activeBar != null)
+            activeBar.Active = false;
+
+        activeBar = bar;
+        if (activeBar != null)
+            activeBar.Active = true;
+    }
+
+
+    public void SetData(ILivingLetterData data)
+    {
+        // Clear past data
+        foreach (var b in bars)
+        {
+            Destroy(b.gameObject);
+        }
+        bars.Clear();
+
+        string text = ArabicAlphabetHelper.PrepareStringForDisplay("لم نرك منذ مدة لم نرك منذ مدة");
+
+        string[] words = text.Split(' ');
+        int wordsCount = words.Length;
+
+        var currentReadingBar = GameObject.Instantiate(readingBarPrefab);
+        currentReadingBar.transform.SetParent(barsStart);
+        currentReadingBar.transform.localPosition = Vector3.zero;
+        currentReadingBar.text.text = words[0];
+        bars.Add(currentReadingBar);
+        SetActiveBar(currentReadingBar);
+
+        for (int i = 1; i < wordsCount; ++i)
+        {
+            var word = words[i];
+
+            var previous = currentReadingBar.text.text;
+            currentReadingBar.text.text = currentReadingBar.text.text + " " + word;
+
+            // Evaluate split
+            if (currentReadingBar.text.GetPreferredValues().x >= 10)
+            {
+                currentReadingBar.text.text = previous;
+
+                currentReadingBar = GameObject.Instantiate(readingBarPrefab);
+                currentReadingBar.transform.SetParent(barsStart);
+                currentReadingBar.transform.localPosition = Vector3.down * distanceBetweenBars;
+                currentReadingBar.text.text = word;
+                bars.Add(currentReadingBar);
+            }
+        }
+    }
+
+    public bool SwitchToNextBar()
+    {
+        if (activeBar != null)
+            activeBar.Complete();
+
+        int nextId = bars.FindIndex((b) => { return b == activeBar; }) + 1;
+
+        if (nextId >= bars.Count)
+        {
+            // Completed!
+            SetActiveBar(null);
+            return true;
+        }
+        else
+        {
+            // Switch to next
+            SetActiveBar(bars[nextId]);
+
+        }
+
+        return false;
+    }
+
+
 
     public ReadingBar PickGlass(Camera main, Vector2 lastPointerPosition)
     {
-        if (activeBar == null)
+        if (!active || activeBar == null)
             return null;
 
         var barCollider = activeBar.glass.GetComponentInChildren<Collider>();
