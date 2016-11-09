029f8b501c8c057918bc4a80cf0415638b33ae57
Davide Barbieri <davide.barbieri@ghostshark.it>
2016-11-09 16:20:47 +0100

    [LL] Automatic pass to idle and back for doAngry/doHooray/doHighFive [LL] Fixed doAngry/doHooray/doHighFive when doing idle alternative [LL] Fixed twirl

---
+++ Assets/_manage/preview_Actors/_scripts/LetterObjectViewTester.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using EA4S;
+
+public class LetterObjectViewTester : MonoBehaviour
+{
+    LetterObjectView letter;
+
+    public LLAnimationStates targetState;
+    public bool doTransition;
+
+    public bool doHooray;
+    public bool doAngry;
+    public bool doHighFive;
+
+    void Start ()
+    {
+        letter = GetComponent<LetterObjectView>();
+	}
+	
+	void Update ()
+    {
+	    if (doTransition)
+        {
+            doTransition = false;
+            letter.SetState(targetState);
+        }
+
+        if (doHooray)
+        {
+            doHooray = false;
+            letter.DoHorray();
+        }
+
+
+        if (doAngry)
+        {
+            doAngry = false;
+            letter.DoAngry();
+        }
+
+
+        if (doHighFive)
+        {
+            doHighFive = false;
+            letter.DoHighFive();
+        }
+    }
+}
