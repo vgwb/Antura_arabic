a75fee4747b6388fb12f0abc4a0da96d5d976df5
Michele <cat.piro@gmail.com>
2016-11-11 17:59:17 +0100

    Added SetupLearnRules to all games.

--- Assets/_games/Balloons/_configurationscripts/BalloonsConfiguration.cs
+++ Assets/_games/Balloons/_configurationscripts/BalloonsConfiguration.cs
@@ -91,6 +91,14 @@
             return builder;
         }
 
+        public MiniGameLearnRules SetupLearnRules()
+        {
+            var rules = new MiniGameLearnRules();
+            // example: a.minigameVoteSkewOffset = 1f;
+            return rules;
+        }
+
+
         #endregion
     }
 }
