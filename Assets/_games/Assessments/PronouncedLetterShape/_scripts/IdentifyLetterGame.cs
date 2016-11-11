a18bad62c48e4c4e7f13bf51537e59cd6726256b
Stefano Cecere <stefano.cecere@krur.com>
2016-11-11 17:03:17 +0100

    [Assessment] init assessment_IdentifyLetter

---
+++ Assets/_games/Assessments/PronouncedLetterShape/_scripts/IdentifyLetterGame.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using EA4S.Assessment;
+using TMPro;
+
+namespace EA4S.IdentifyLetter
+{
+    /// <summary>
+    /// This game do not have any localization peculiarity. Touch the prounounced
+    /// letter to make a point, more points => more reward
+    /// </summary>
+    public class IdentifyLetterGame : MiniGame
+    {
+        [Header("Managers")]
+        public QuestionController questionController;
+
+        public IdentifyLetterIntroState IntroductionState { get; private set; }
+        public IdentifyLetterQuestionState QuestionState { get; private set; }
+        public PlayGameState PlayState { get; private set; }
+        public IdentifyLetterResultState ResultState { get; private set; }
+        public ScoreCounter score { get; private set; }
+        public TimeEngine Time { get; private set; }
+
+        protected override void OnInitialize( IGameContext context)
+        {
+            IntroductionState = new IdentifyLetterIntroState( this);
+            QuestionState = new IdentifyLetterQuestionState( this);
+            PlayState = new PlayGameState( this);
+            ResultState = new IdentifyLetterResultState( this);
+            Time = new TimeEngine();
+            score = new ScoreCounter();
+        }
+
+        protected override IGameState GetInitialState()
+        {
+            return IntroductionState;
+        }
+
+        protected override IGameConfiguration GetConfiguration()
+        {
+            return IdentifyLetterConfiguration.Instance;
+        }
+    }
+}
