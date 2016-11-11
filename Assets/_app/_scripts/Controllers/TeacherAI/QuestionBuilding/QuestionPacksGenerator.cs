93a031bd26c1726b4d02021f670374d4eb8cf98f
Michele <cat.piro@gmail.com>
2016-11-11 11:50:56 +0100

    Fixed bugs with MakeFriends' question builder.

--- Assets/_app/_scripts/Controllers/TeacherAI/QuestionBuilding/QuestionPacksGenerator.cs
+++ Assets/_app/_scripts/Controllers/TeacherAI/QuestionBuilding/QuestionPacksGenerator.cs
@@ -33,10 +33,21 @@ namespace EA4S
             foreach(var questionPackData in questionPackDataList)
             {
                 ILivingLetterData ll_question = questionPackData.question != null ? questionPackData.question.ConvertToLivingLetterData() : null;
+                List<ILivingLetterData> ll_questions = questionPackData.questions != null ? questionPackData.questions.ConvertAll(x => x.ConvertToLivingLetterData()) : null;
                 List<ILivingLetterData> ll_wrongAnswers = questionPackData.wrongAnswers != null ? questionPackData.wrongAnswers.ConvertAll(x => x.ConvertToLivingLetterData()) : null;
                 List<ILivingLetterData> ll_correctAnswers = questionPackData.correctAnswers != null ? questionPackData.correctAnswers.ConvertAll(x => x.ConvertToLivingLetterData()) : null;
-                IQuestionPack questionPack = new FindRightDataQuestionPack(ll_question, ll_wrongAnswers, ll_correctAnswers);
-                questionPackList.Add(questionPack);
+                IQuestionPack pack;
+
+                // Conversion based on what kind of question we have
+                // @todo: at least on the teacher's side, this could be simplified by always using a LIST of questions
+                if (ll_questions != null && ll_questions.Count > 0)
+                {
+                    pack = new FindRightDataQuestionPack(ll_questions, ll_wrongAnswers, ll_correctAnswers);
+                } else
+                {
+                    pack = new FindRightDataQuestionPack(ll_question, ll_wrongAnswers, ll_correctAnswers);
+                }
+                questionPackList.Add(pack);
             }
             return questionPackList;
         }
