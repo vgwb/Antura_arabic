86b3b19c03740ec6d340fc714d4f1809532fd267
Stefano Cecere <stefano.cecere@krur.com>
2016-11-12 07:58:30 +0100

    [Book] init PlayerBook scene

---
+++ Assets/_app/_scripts/Controllers/Book/BookManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using EA4S;
+using ModularFramework.Core;
+
+namespace EA4S
+{
+    public class BookManager : MonoBehaviour
+    {
+
+
+
+        public void OpenMap()
+        {
+            GameManager.Instance.Modules.SceneModule.LoadSceneWithTransition("app_Map");
+        }
+    }
+}
