64945c1ba963d091518a9bd0e72a3f81ab854c49
Demigiant <daniele.giardini@gmail.com>
2016-11-12 19:32:17 +0100

    [ProfileSelector] Progress

--- Assets/_app/_scripts/Controllers/UI/ProfileSelector/ProfileSelectorUI.cs
+++ Assets/_app/_scripts/Controllers/UI/ProfileSelector/ProfileSelectorUI.cs
@@ -1,6 +1,8 @@
 ﻿// Author: Daniele Giardini - http://www.demigiant.com
 // Created: 2016/11/12
 
+using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace EA4S
@@ -15,7 +17,9 @@ namespace EA4S
 
         public static ProfileSelectorUI I;
         int maxProfiles;
+        List<PlayerProfile> playerProfiles = new List<PlayerProfile>();
         ProfileSelectorAvatarButton[] avatarButtons;
+        Tween btAddTween;
 
         #region Unity
 
@@ -27,9 +31,65 @@ namespace EA4S
             maxProfiles = avatarButtons.Length;
         }
 
+        void Start()
+        {
+            // TODO Get profiles
+            Setup();
+
+            btAddTween = BtAdd.transform.DORotate(new Vector3(0, 0, -45), 0.3f).SetAutoKill(false).Pause()
+                .SetEase(Ease.OutBack);
+
+            // Listeners
+            BtAdd.Bt.onClick.AddListener(()=> OnClick(BtAdd));
+        }
+
         void OnDestroy()
         {
             if (I == this) I = null;
+            btAddTween.Kill();
+        }
+
+        #endregion
+
+        #region Methods
+
+        // Layout with current profiles
+        void Setup()
+        {
+            AvatarSelector.gameObject.SetActive(false);
+
+            int totProfiles = playerProfiles.Count;
+            for (int i = 0; i < avatarButtons.Length; ++i) {
+                if (i >= totProfiles) avatarButtons[i].gameObject.SetActive(false);
+                else {
+                    // TODO
+                }
+            }
+
+            if (totProfiles == 0) {
+                BtAdd.Pulse();
+                BtPlay.gameObject.SetActive(false);
+            }
+        }
+
+        #endregion
+
+        #region Callbacks
+
+        void OnClick(UIButton _bt)
+        {
+            if (_bt == BtAdd) {
+                _bt.StopPulsing();
+                if (AvatarSelector.IsShown) {
+                    btAddTween.PlayBackwards();
+                    AvatarSelector.Hide();
+                } else {
+                    btAddTween.PlayForward();
+                    AvatarSelector.Show();
+                }
+            } else {
+                _bt.AnimateClick();
+            }
         }
 
         #endregion
--- Assets/_app/_scripts/Controllers/UI/UIButton.cs
+++ Assets/_app/_scripts/Controllers/UI/UIButton.cs
@@ -19,22 +19,32 @@ namespace EA4S
         Button fooBt;
         Image btImg, ico;
         Color defColor;
-        Tween clickTween;
+        Tween clickTween, pulseTween;
+
+        #region Unity
 
         void Awake()
         {
             clickTween = this.transform.DOPunchScale(new Vector3(0.2f, 0.2f, 0.2f), 0.35f).SetAutoKill(false).SetUpdate(true).Pause();
+            pulseTween = this.transform.DOScale(this.transform.localScale * 1.1f, 0.3f).SetAutoKill(false).SetUpdate(true).Pause()
+                .SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutQuad);
         }
 
         void OnDestroy()
         {
             clickTween.Kill();
+            pulseTween.Kill();
         }
+
+        #endregion
+
+        #region Public Methods
 
         public void Toggle(bool _activate)
         {
             IsToggled = _activate;
 
+            pulseTween.Rewind();
             if (btImg == null) {
                 btImg = this.GetComponent<Image>();
                 defColor = btImg.color;
@@ -45,9 +55,25 @@ namespace EA4S
             ico.SetAlpha(_activate ? 1 : 0.4f);
         }
 
+        /// <summary>
+        /// Pulsing stops automatically when the button is toggled or clicked (via <see cref="AnimateClick"/>)
+        /// </summary>
+        public void Pulse()
+        {
+            pulseTween.PlayForward();
+        }
+
+        public void StopPulsing()
+        {
+            pulseTween.Rewind();
+        }
+
         public void AnimateClick()
         {
+            pulseTween.Rewind();
             clickTween.Restart();
         }
+
+        #endregion
     }
 }
