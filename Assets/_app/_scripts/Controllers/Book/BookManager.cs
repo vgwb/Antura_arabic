917d29c8a209d6216af2622d7b9debca1928ebf1
Stefano Cecere <stefano.cecere@krur.com>
2016-11-13 08:29:08 +0100

    [Drawings] optimized Atlas and test rendering in Book scene

--- Assets/_app/_scripts/Controllers/Book/BookManager.cs
+++ Assets/_app/_scripts/Controllers/Book/BookManager.cs
@@ -6,7 +6,16 @@ namespace EA4S
 {
     public class BookManager : MonoBehaviour
     {
+        [Header("Scene Setup")]
+        public Music SceneMusic;
 
+        void Start()
+        {
+            GlobalUI.ShowPauseMenu(false);
+            AudioManager.I.PlayMusic(SceneMusic);
+
+            SceneTransitioner.Close();
+        }
 
 
         public void OpenMap()
