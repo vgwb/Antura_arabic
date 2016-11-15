54e2c563e970536ac988d95449604d7a416fa3ea
Demigiant <daniele.giardini@gmail.com>
2016-11-15 12:52:29 +0100

    [GlobalUI] ActionFeedback now uses new UI art, has different animation, and doesn't stay on screen when not necessary

---
+++ Assets/_manage/preview_UI/_scripts/Tester_GlobalUI.cs
@@ -0,0 +1,19 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2016/11/15
+
+using UnityEngine;
+
+namespace EA4S.Test
+{
+    public class Tester_GlobalUI : MonoBehaviour
+    {
+        #region ActionFeedback
+
+        public void Show(bool _feedback)
+        {
+            GlobalUI.I.ActionFeedback.Show(_feedback);
+        }
+
+        #endregion
+    }
+}
