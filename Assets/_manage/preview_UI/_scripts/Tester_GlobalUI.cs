ba86653095f1e7653d1eee570ac090bedbcbe547
Demigiant <daniele.giardini@gmail.com>
2016-11-15 14:53:59 +0100

    [GlobalUI][WidgetSubtitles] Updated to new UI art and different animations

--- Assets/_manage/preview_UI/_scripts/Tester_GlobalUI.cs
+++ Assets/_manage/preview_UI/_scripts/Tester_GlobalUI.cs
@@ -9,9 +9,35 @@ namespace EA4S.Test
     {
         #region ActionFeedback
 
-        public void Show(bool _feedback)
+        public void ActionFeedback_Show(bool _feedback)
         {
             GlobalUI.I.ActionFeedback.Show(_feedback);
+        }
+
+        #endregion
+
+        #region WidgetSubtitles
+
+        public void Subtitles_DisplaySentence(bool _isKeeper)
+        {
+            GlobalUI.WidgetSubtitles.DisplaySentence(RandomTextId(), 3f, _isKeeper);
+        }
+
+        public void Subtitles_Close()
+        {
+            GlobalUI.WidgetSubtitles.Close();
+        }
+
+        string RandomTextId()
+        {
+            int rnd = UnityEngine.Random.Range(0, 5);
+            switch (rnd) {
+            case 0: return TextID.ASSESSMENT_START_A1.ToString();
+            case 1: return TextID.ASSESSMENT_START_A2.ToString();
+            case 2: return TextID.ASSESSMENT_START_A3.ToString();
+            case 3: return TextID.ASSESSMENT_RESULT_GOOD.ToString();
+            default: return TextID.ASSESSMENT_RESULT_INTRO.ToString();
+            }
         }
 
         #endregion
