d486bd97830be80408229fd4fdb9c2b1d578a7b6
Brago <bragolab@gmail.com>
2016-11-15 17:36:08 +0100

    - MinigameAPI refactoring with better code organization.

---
+++ Assets/_app/_scripts/MinigameAPI/FindRightDataQuestionPack.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EA4S.API {
+    /// <summary>
+    /// Data Pack for "find right question" mechanics.
+    /// One data question data, many right answare data, many answare data.
+    /// </summary>
+    /// <seealso cref="EA4S.IQuestionPack" />
+    public class FindRightDataQuestionPack : IQuestionPack {
+        IEnumerable<ILivingLetterData> questionsSentences;
+        IEnumerable<ILivingLetterData> wrongAnswersSentence;
+        IEnumerable<ILivingLetterData> correctAnswersSentence;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FindRightDataQuestionPack"/> class.
+        /// </summary>
+        /// <param name="questionSentence">The question sentence.</param>
+        /// <param name="wrongAnswersSentence">The wrong answers sentence.</param>
+        /// <param name="correctAnswersSentence">The correct answers sentence.</param>
+        public FindRightDataQuestionPack(ILivingLetterData questionSentence, IEnumerable<ILivingLetterData> wrongAnswersSentence, IEnumerable<ILivingLetterData> correctAnswersSentence) {
+            this.questionsSentences = new List<ILivingLetterData>() { questionSentence };
+            this.wrongAnswersSentence = wrongAnswersSentence;
+            this.correctAnswersSentence = correctAnswersSentence;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FindRightDataQuestionPack"/> class.
+        /// </summary>
+        /// <param name="questionsSentences">The questions sentences.</param>
+        /// <param name="wrongAnswersSentence">The wrong answers sentence.</param>
+        /// <param name="correctAnswersSentence">The correct answers sentence.</param>
+        public FindRightDataQuestionPack(IEnumerable<ILivingLetterData> questionsSentences, IEnumerable<ILivingLetterData> wrongAnswersSentence, IEnumerable<ILivingLetterData> correctAnswersSentence) {
+            this.questionsSentences = questionsSentences;
+            this.wrongAnswersSentence = wrongAnswersSentence;
+            this.correctAnswersSentence = correctAnswersSentence;
+        }
+
+        ILivingLetterData IQuestionPack.GetQuestion() {
+            return questionsSentences.First();
+        }
+
+        public IEnumerable<ILivingLetterData> GetQuestions() {
+            return questionsSentences;
+        }
+
+        IEnumerable<ILivingLetterData> IQuestionPack.GetWrongAnswers() {
+            return wrongAnswersSentence;
+        }
+
+        public IEnumerable<ILivingLetterData> GetCorrectAnswers() {
+            return correctAnswersSentence;
+        }
+
+
+    }
+}
