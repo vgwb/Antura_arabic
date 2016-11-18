d033c5e475cd71cfe77175c60f0d9feab197794b
Michele <cat.piro@gmail.com>
2016-11-18 16:59:37 +0100

    Added Teacher preview scene. Updated SelectionParametrs to support getAllData. Fixed issues with BalloonsConfiguration. Added 'ignore journey' parameters to some builders.

--- Assets/_games/Balloons/_configurationscripts/BalloonsConfiguration.cs
+++ Assets/_games/Balloons/_configurationscripts/BalloonsConfiguration.cs
@@ -81,7 +81,7 @@
                     builder = new WordsWithLetterQuestionBuilder(nPacks, nCorrect, nWrong);
                     break;  
                 case BalloonsVariation.Spelling:
-                    builder = new LettersInWordQuestionBuilder(nPacks, nCorrect, nWrong);
+                    builder = new LettersInWordQuestionBuilder(nPacks, useAllCorrectLetters:true, nWrong:nWrong);
                     break;
                 case BalloonsVariation.Words:
                     builder = new RandomWordsQuestionBuilder(nPacks, 1, nWrong, true);
