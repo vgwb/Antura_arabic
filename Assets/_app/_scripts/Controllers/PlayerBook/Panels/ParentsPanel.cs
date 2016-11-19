b87aa8febf62a3c8654479b2de53ddb0d20b5e61
Stefano Cecere <stefano.cecere@krur.com>
2016-11-19 08:34:34 +0100

    [PlayerBook] init Letters, Phrases, Minigames and LearningBlocks areas

--- Assets/_app/_scripts/Controllers/PlayerBook/Panels/ParentsPanel.cs
+++ Assets/_app/_scripts/Controllers/PlayerBook/Panels/ParentsPanel.cs
@@ -1,15 +1,43 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
+using EA4S.Db;
 
-public class ParentsPanel : MonoBehaviour {
+namespace EA4S
+{
+    public class ParentsPanel : MonoBehaviour
+    {
+        [Header("Prefabs")]
+        public GameObject ButtonPrefab;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        [Header("References")]
+        public GameObject ElementsContainer;
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        void OnEnable()
+        {
+            InitUI();
+        }
+
+        void InitUI()
+        {
+            GameObject btnGO;
+
+            //// Words
+            foreach (Transform t in ElementsContainer.transform) {
+                Destroy(t.gameObject);
+            }
+
+            foreach (LearningBlockData word in AppManager.Instance.DB.GetAllLearningBlockData()) {
+                btnGO = Instantiate(ButtonPrefab);
+                btnGO.transform.SetParent(ElementsContainer.transform, false);
+                btnGO.GetComponentInChildren<Text>().text = word.Title_En;
+            }
+        }
+    }
 }
