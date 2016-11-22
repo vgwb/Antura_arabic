252ce45226ad037a45cf72adb2f4138e0ea2600e
Joseph Saade <Joseph Saade>
2016-11-22 23:31:18 +0200

    [TakeMeHome] Use Teach + Letter Provider

---
+++ Assets/_games/TakeMeHome/_scripts/TakeMeHomeLettersProvider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EA4S.TakeMeHome
+{
+    /// <summary>
+    /// Letter provider sample
+    /// </summary>
+    public class TakeMeHomeLettersProvider : ILivingLetterDataProvider
+    {
+        public List<LL_LetterData> letters;
+        public TakeMeHomeLettersProvider()
+        {
+            letters = new List<LL_LetterData>();
+            for (int i =0; i < 7;++i)
+            {
+                letters.Add(AppManager.Instance.Teacher.GetRandomTestLetterLL());
+            }
+        }
+
+        public ILivingLetterData GetNextData()
+        {
+            LL_LetterData ld = letters[0];
+            letters.RemoveAt(0);
+            return ld;
+        }
+    }
+}
