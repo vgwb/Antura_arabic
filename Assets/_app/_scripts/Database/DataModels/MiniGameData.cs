5f48f0a45850e7cc6a0a14196ad27de31b43181f
Stefano Cecere <stefano.cecere@krur.com>
2016-11-22 01:01:01 +0100

    [audio] 300 dialog audiofiles ready to be played

--- Assets/_app/_scripts/Database/DataModels/MiniGameData.cs
+++ Assets/_app/_scripts/Database/DataModels/MiniGameData.cs
@@ -27,6 +27,12 @@ namespace EA4S.Db
             return string.Format("[Minigame: id={0}, type={4}, available={1},  title_en={2}, title_ar={3}]", GetId(), Available, Title_En, Title_Ar, Type.ToString());
         }
 
+        public string GetTitleSoundFilename()
+        {
+            return GetId() + "_Title";
+        }
+
+
         public string GetIconResourcePath()
         {
             return "Images/GameIcons/minigame_Ico_" + Main;
