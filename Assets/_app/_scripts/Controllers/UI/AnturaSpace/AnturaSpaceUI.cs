b731faefe4252b8dd244fe8ac62bb9b5592622d2
Demigiant <daniele.giardini@gmail.com>
2016-11-24 18:24:32 +0100

    [AnturaSpaceUI] Setting things up

---
+++ Assets/_app/_scripts/Controllers/UI/AnturaSpace/AnturaSpaceUI.cs
@@ -0,0 +1,12 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2016/11/24
+
+using UnityEngine;
+
+namespace EA4S
+{
+    public class AnturaSpaceUI : MonoBehaviour
+    {
+        
+    }
+}
