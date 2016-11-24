ad286fff3588bd3199bec8584b00beda587fccab
Hussam Kazah <nu.urth@gmail.com>
2016-11-25 00:32:51 +0200

    [Dancing Dots] Getting all letters from Teacher - Adjusted local provider

--- Assets/_games/DancingDots/_scripts/DancingDotsQuestionsManager.cs
+++ Assets/_games/DancingDots/_scripts/DancingDotsQuestionsManager.cs
@@ -1,34 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
 namespace EA4S.DancingDots
 {
 	public class DancingDotsQuestionsManager
 	{
-        string dotlessLetters = /*"ض ث ق ف غ خ ج ش ي ب ت ن ة ظ"*/ " - ﻻ لأ ﺉ آ إ ٶ أ ا ى ر س ل ص ع ه ح د م ك ط ئ ء ؤ و", prevLetter = "";
+		private IQuestionProvider provider;
+
+		public DancingDotsQuestionsManager()
+		{
+		    provider = DancingDotsConfiguration.Instance.Questions;
+		}
 
 		public ILivingLetterData getNewLetter()
 		{
+			var question = provider.GetNextQuestion();
 
-			ILivingLetterData newLetter = null;
+			return question.GetCorrectAnswers().First();
 
-			do
-			{
-				if(newLetter!=null)
-					prevLetter = newLetter.TextForLivingLetter;
-				IQuestionProvider newQuestionProvider = DancingDotsConfiguration.Instance.Questions;
-				IQuestionPack nextQuestionPack = newQuestionProvider.GetNextQuestion(); 
-
-				foreach (ILivingLetterData letterData in nextQuestionPack.GetCorrectAnswers())
-				{
-					newLetter = letterData;
-
-				}
-			}
-			while (!dotlessLetters.Contains(newLetter.TextForLivingLetter) || newLetter.TextForLivingLetter == prevLetter);
-
-			//ILivingLetterData newLetter = ((SampleQuestionPack)nextQuestionPack).GetQuestions();
-
-			return newLetter;
 		}
 	}
 }
