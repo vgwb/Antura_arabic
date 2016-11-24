2540fa7db1b9ee80e7b6274ad8f41c3fabc2e9a1
ModarD <skullyubel@gmail.com>
2016-11-25 00:05:56 +0200

    [SickLetters] Added support for dotless letters , Refactored data retrieval

--- Assets/_games/SickLetters/_scripts/QuestionsManager.cs
+++ Assets/_games/SickLetters/_scripts/QuestionsManager.cs
@@ -4,29 +4,17 @@ namespace EA4S.SickLetters
 {
     public class QuestionsManager
     {
-        string dotlessLetters = " - ﻻ لأ ﺉ آ ٶ ى ر س ل ص ع ه ح د م ك ط ئ ء ؤ و �ة", prevLetter = "";
-
         public ILivingLetterData getNewLetter()
         {
-
             ILivingLetterData newLetter = null;
-            
-            do
+
+            IQuestionProvider newQuestionProvider = SickLettersConfiguration.Instance.Questions;
+            IQuestionPack nextQuestionPack = newQuestionProvider.GetNextQuestion();
+
+            foreach (ILivingLetterData letterData in nextQuestionPack.GetCorrectAnswers())
             {
-                if(newLetter!=null)
-                    prevLetter = newLetter.TextForLivingLetter;
-                IQuestionProvider newQuestionProvider = SickLettersConfiguration.Instance.Questions;
-                IQuestionPack nextQuestionPack = newQuestionProvider.GetNextQuestion(); 
-
-                foreach (ILivingLetterData letterData in nextQuestionPack.GetCorrectAnswers())
-                {
-                    newLetter = letterData;
-                    
-                }
+                newLetter = letterData;
             }
-            while (dotlessLetters.Contains(newLetter.TextForLivingLetter) || newLetter.TextForLivingLetter == prevLetter);
-
-            //ILivingLetterData newLetter = ((SampleQuestionPack)nextQuestionPack).GetQuestions();
 
             return newLetter;
         }
