bcda5abafe47dfa58b416aa960c96a6c6176b256
Stefano Cecere <stefano.cecere@krur.com>
2016-11-27 01:26:04 +0100

    [clean] simplified AppConstants.cs

--- Assets/_app/_scripts/_App/AppSettings.cs
+++ Assets/_app/_scripts/_App/AppSettings.cs
@@ -3,6 +3,16 @@
 namespace EA4S
 {
 
+    public static class AppConstants
+    {
+        public const string AppVersion = "0.11.28b";
+
+#if UNITY_EDITOR
+        public const string UrlGithubRepository = "https://github.com/vgwb/EA4S_Antura_U3D";
+        public const string UrlTrello = "https://trello.com/b/ltLndaQI/ea4s-beta";
+#endif
+    }
+
     /// <summary>
     /// Game Setting Extension class.
     /// </summary>
