41965d8d3f0cf51d65fa489ad21bdbc2a7a34491
Stefano Cecere <stefano.cecere@krur.com>
2016-11-28 00:53:14 +0100

    [Map] Book icon has the Player Avatar image

--- Assets/_app/_scripts/Book/UI/OpenPlayerBookScene.cs
+++ Assets/_app/_scripts/Book/UI/OpenPlayerBookScene.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 namespace EA4S
 {
     public class OpenPlayerBookScene : MonoBehaviour, IPointerClickHandler
     {
+        void Start()
+        {
+            Debug.Log("current player avatar is: " + AppManager.I.Player.AvatarId);
+            GetComponent<Image>().sprite = AppManager.I.Player.GetAvatar();
+        }
 
         public void OnPointerClick(PointerEventData eventData)
         {
