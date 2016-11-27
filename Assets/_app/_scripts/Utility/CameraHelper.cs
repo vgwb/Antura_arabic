90468e8f794abaa32cc8740080f9d8948eae5f1f
Demigiant <daniele.giardini@gmail.com>
2016-11-27 18:28:17 +0100

    [PlaySessionResultUI] Rewards now scale correctly using same logic as Antura Sapce

--- Assets/_app/_scripts/Utility/CameraHelper.cs
+++ Assets/_app/_scripts/Utility/CameraHelper.cs
@@ -10,6 +10,7 @@ namespace EA4S
     {
         public static void FitRewardToUICamera(Transform _trans, Camera _cam, bool _flip = false)
         {
+            _trans.localPosition = Vector3.zero;
             if (_flip) _trans.eulerAngles = new Vector3(-45, 60, 150);
             Bounds bounds = new Bounds(Vector3.zero, new Vector3(-1, -1, -1));
             Renderer[] rs = _trans.GetComponentsInChildren<Renderer>(true);
@@ -18,6 +19,7 @@ namespace EA4S
                 if (bounds.size.x < 0) bounds = r.bounds;
                 else bounds.Encapsulate(r.bounds);
             }
+            Debug.Log(bounds);
             Vector3 diff = new Vector3(_trans.position.x - bounds.center.x, _trans.position.y - bounds.center.y, _trans.position.z - bounds.center.z);
             _trans.Translate(diff, Space.World);
             float frustumVal = bounds.size.y >= bounds.size.x ? bounds.size.y : bounds.size.x / _cam.aspect;
