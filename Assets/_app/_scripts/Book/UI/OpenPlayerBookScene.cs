a421a19149d317b00ce3154c0f3b5cca70e902dc
Demigiant <daniele.giardini@gmail.com>
2016-11-28 16:59:04 +0100

    [Map] Adapted Antura button and BtnOpenPlayerBook prefab to UI graphics

--- Assets/_app/_scripts/Book/UI/OpenPlayerBookScene.cs
+++ Assets/_app/_scripts/Book/UI/OpenPlayerBookScene.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using DG.DeExtensions;
+using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
@@ -6,10 +7,13 @@ namespace EA4S
 {
     public class OpenPlayerBookScene : MonoBehaviour, IPointerClickHandler
     {
+        public Image IcoAvatar;
+
         void Start()
         {
             Debug.Log("current player avatar is: " + AppManager.I.Player.AvatarId);
-            GetComponent<Image>().sprite = AppManager.I.Player.GetAvatar();
+//            GetComponent<Image>().sprite = AppManager.I.Player.GetAvatar();
+            IcoAvatar.sprite = AppManager.I.Player.GetAvatar();
         }
 
         public void OnPointerClick(PointerEventData eventData)
