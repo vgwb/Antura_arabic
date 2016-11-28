3b28b7b240de7440cc6c76d7c038444532eaa00c
Fouad Tabsh <tabshf@gmail.com>
2016-11-28 18:47:54 +0200

    [MixedLetters] Fix remaining issues.

--- Assets/_games/MixedLetters/_scripts/VictimLLController.cs
+++ Assets/_games/MixedLetters/_scripts/VictimLLController.cs
@@ -5,6 +5,8 @@ namespace EA4S.MixedLetters
 {
     public class VictimLLController : MonoBehaviour
     {
+        private const float LOOK_TOWARDS_ANTURA_TIME = 0.33f;
+
         public static VictimLLController instance;
         public LetterObjectView letterObjectView;
 
@@ -15,16 +17,6 @@ namespace EA4S.MixedLetters
             instance = this;
         }
 
-        void Start()
-        {
-
-        }
-
-        void Update()
-        {
-
-        }
-
         public void SetCustomText(string text)
         {
             if (letterObjectView.Label.enabled == false)
@@ -33,6 +25,34 @@ namespace EA4S.MixedLetters
             }
 
             letterObjectView.Label.SetText(text);
+        }
+
+        public void LookTowardsAntura()
+        {
+            StartCoroutine(LookTowardsAnturaCoroutine());
+        }
+
+        private IEnumerator LookTowardsAnturaCoroutine()
+        {
+            yield return new WaitForSeconds(0.1f);
+
+            letterObjectView.SetState(LLAnimationStates.LL_still);
+
+            bool anturaEntersFromLeft = transform.position.x > 0;
+
+            float targetAngle = 180 + 80 * (anturaEntersFromLeft ? 1 : -1);
+
+            float timeElapsed = 0;
+            float sinFactor = 2 * Mathf.PI * Mathf.Pow(LOOK_TOWARDS_ANTURA_TIME * 4, -1);
+
+            while(timeElapsed < LOOK_TOWARDS_ANTURA_TIME)
+            {
+                transform.rotation = Quaternion.Euler(0, Mathf.Lerp(180, targetAngle, Mathf.Sin(sinFactor * timeElapsed)), 0);
+                timeElapsed += Time.fixedDeltaTime;
+                yield return new WaitForFixedUpdate();
+            }
+
+            letterObjectView.DoSmallJump();
         }
 
         public void SetPosition(Vector3 position)
@@ -48,6 +68,12 @@ namespace EA4S.MixedLetters
         public void Disable()
         {
             gameObject.SetActive(false);
+        }
+
+        public void Reset()
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            letterObjectView.SetState(LLAnimationStates.LL_idle);
         }
 
         public void DoHooray()
