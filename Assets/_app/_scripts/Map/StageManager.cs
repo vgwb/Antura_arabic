a346087f3df2f9e62a0dbc5079f568ad3eedddeb
Elena Blanes <elenita_bf@hotmail.com>
2016-11-29 17:19:15 +0100

    [MapScene] fixed some bugs + not showing buttons when max and initial possition

--- Assets/_app/_scripts/Map/StageManager.cs
+++ Assets/_app/_scripts/Map/StageManager.cs
@@ -11,8 +11,16 @@ namespace EA4S
         public GameObject[] cameras;
         public GameObject[] miniMaps;
         public GameObject letter;
+
+        [Header("UIButtons")]
         public GameObject leftStageButton;
         public GameObject rightStageButton;
+        public GameObject rightMovementButton;
+        public GameObject leftMovementButton;
+        public GameObject playButton;
+        public GameObject bookButton;
+        public GameObject anturaButton;
+
         int s, i,previousStage;
         bool inTransition;
         void Awake()
@@ -39,6 +47,64 @@ namespace EA4S
 
             StartCoroutine("ResetPosLetter");  
         }
+        void Start()
+        {
+            /* FIRST CONTACT FEATURE */
+            if (AppManager.I.Player.IsFirstContact())
+            {
+                DesactivateUI();
+                FirstContactBehaviour();
+            }
+            /* --------------------- */
+            else ActivateUI();
+        }
+
+        void Update()
+        {
+            // Remove this with First Contact Temp Behaviour
+            UpdateTimer();
+        }
+
+        #region First Contact Session        
+        /// <summary>
+        /// Firsts the contact behaviour.
+        /// Put Here logic for first contact only situations.
+        /// </summary>
+        void FirstContactBehaviour()
+        {
+
+            if (AppManager.I.Player.IsFirstContact(1))
+            {
+                // First contact step 1:
+                #region Temp Behaviour (to be deleted)
+                countDown.Start();
+                #endregion
+                // ..and set first contact done.
+                AppManager.I.Player.FirstContactPassed();
+                anturaButton.SetActive(true);
+                Debug.Log("First Contact Step1 finished! Go to Antura Space!");
+            }
+            else if (AppManager.I.Player.IsFirstContact(2))
+            {
+                // First contact step 2:
+
+                // ..and set first contact done.
+                AppManager.I.Player.FirstContactPassed(2);
+                Debug.Log("First Contact Step2 finished! Good Luck!");
+            }
+
+        }
+        #region Temp Behaviour (to be deleted)
+        CountdownTimer countDown = new CountdownTimer(1f);
+        void OnEnable() { countDown.onTimesUp += CountDown_onTimesUp; }
+        void OnDisable() { countDown.onTimesUp -= CountDown_onTimesUp; }
+        private void CountDown_onTimesUp() { NavigationManager.I.GoToScene(AppScene.Rewards); }
+        private void UpdateTimer() { countDown.Update(Time.deltaTime); }
+        #endregion
+
+        #endregion
+
+
         public void StageLeft()
         {
             int numberStage = AppManager.I.Player.CurrentJourneyPosition.Stage;
@@ -128,11 +194,33 @@ namespace EA4S
             }
 
         }
+        void DesactivateUI()
+        {
+            leftStageButton.SetActive(false);
+            rightStageButton.SetActive(false);
+            rightMovementButton.SetActive(false);
+            leftMovementButton.SetActive(false);
+            playButton.SetActive(false);
+            bookButton.SetActive(false);
+            anturaButton.SetActive(false);
+        }
+        void ActivateUI()
+        {
+            leftStageButton.SetActive(true);
+            rightStageButton.SetActive(true);
+            rightMovementButton.SetActive(true);
+            leftMovementButton.SetActive(true);
+            playButton.SetActive(true);
+            bookButton.SetActive(true);
+            anturaButton.SetActive(true);
+        }
 
         /*void DesactivateMap()
         {
             stages[previousStage].SetActive(false);
             inTransition = false;
         }*/
+
+
     }
 }
