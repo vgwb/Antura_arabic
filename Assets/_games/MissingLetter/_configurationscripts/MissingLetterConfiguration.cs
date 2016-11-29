c81ff071e96bc160b7378e6dca2d6dd5c025204e
Enrico Pernigo <pernigoe@gmail.com>
2016-11-29 23:17:15 +0100

    [MissingLetter] Update game Variation

--- Assets/_games/MissingLetter/_configurationscripts/MissingLetterConfiguration.cs
+++ Assets/_games/MissingLetter/_configurationscripts/MissingLetterConfiguration.cs
@@ -1,12 +1,23 @@
 ﻿namespace EA4S.MissingLetter
 {
+    //enum game type
+    public enum MissingLetterVariation : int
+    {
+        WORD = 1,
+        PHRASE = 2
+    }
+
     public class MissingLetterConfiguration : IGameConfiguration
     {
+
+
         // Game configuration
         public IGameContext Context { get; set; }
         public IQuestionProvider Questions { get; set; }
 
         public float Difficulty { get; set; }
+        public MissingLetterVariation Variation { get; set; }
+
 
         /////////////////
         // Singleton Pattern
@@ -27,9 +38,19 @@
             // Default values
             // THESE SETTINGS ARE FOR SAMPLE PURPOSES, THESE VALUES MUST BE SET BY GAME CORE
             Questions = new SampleQuestionProvider();
-
             Context = new SampleGameContext();
+
+            Variation = MissingLetterVariation.WORD;
             Difficulty = 0.5f;
+        }
+
+        public static void SetConfiguration(float _difficulty, int _variation)
+        {
+            instance = new MissingLetterConfiguration()
+            {
+                Difficulty = _difficulty,
+                Variation = (MissingLetterVariation)_variation,
+            };
         }
 
         public IQuestionBuilder SetupBuilder() {
@@ -40,7 +61,15 @@
             int nWrong = 5;
 
             var builderParams = new Teacher.QuestionBuilderParameters();
-            builder = new LettersInWordQuestionBuilder(nPacks, nCorrect, nWrong, parameters: builderParams);
+            switch (Variation)
+            {
+                case MissingLetterVariation.WORD:
+                    builder = new LettersInWordQuestionBuilder(nPacks, nCorrect, nWrong, parameters: builderParams);
+                    break;
+                case MissingLetterVariation.PHRASE:
+                    builder = new WordsInPhraseQuestionBuilder(nPacks, nCorrect, nWrong, parameters: builderParams);
+                    break;
+            }
 
             return builder;
         }
