5179a42678211af54bba41bb93eea8706df611d6
George H <george.hbr@gmail.com>
2016-11-30 15:08:22 +0200

    [Balloons] Better round lose animation

--- Assets/_games/Balloons/_scripts/RoundResultAnimator.cs
+++ Assets/_games/Balloons/_scripts/RoundResultAnimator.cs
@@ -8,7 +8,8 @@ namespace EA4S.Balloons
     {
         public LetterObjectView LLPrefab;
         public ParticleSystem vfx;
-        public Vector3 wrongMarkPosition;
+        public Vector3 wrongMarkPosition1;
+        public Vector3 wrongMarkPosition2;
         public Vector3 onscreenPosition;
         public Vector3 offscreenPosition;
         public float moveDuration;
@@ -38,7 +39,8 @@ namespace EA4S.Balloons
                 LLPrefab.Init(livingLetterData);
             }
             LLPrefab.DoAngry();
-            TutorialUI.MarkNo(wrongMarkPosition, TutorialUI.MarkSize.Huge);
+            TutorialUI.MarkNo(wrongMarkPosition1, TutorialUI.MarkSize.Huge);
+            TutorialUI.MarkNo(wrongMarkPosition2, TutorialUI.MarkSize.Huge);
 
             StartCoroutine(Move_Coroutine(offscreenPosition, onscreenPosition, moveDuration));
         }
