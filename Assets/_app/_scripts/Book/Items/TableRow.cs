1d4eedccfdccb08f76b14ec0f0c950b91a0fbba1
Stefano Cecere <stefano.cecere@krur.com>
2016-12-01 15:32:33 +0100

    [book] player info table

---
+++ Assets/_app/_scripts/Book/Items/TableRow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EA4S
+{
+    public class TableRow : MonoBehaviour
+    {
+        public TextRender TxTitle;
+        public TextRender TxSubtitle;
+        public TextRender TxValue;
+
+        public void Init(string _title, string _value, string _subtitle = "")
+        {
+            TxTitle.setText(_title);
+            TxSubtitle.setText(_subtitle);
+            TxValue.setText(_value);
+        }
+    }
+}
---
+++ Assets/_app/_scripts/Book/UI/InfoTable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace EA4S
+{
+    public class InfoTable : MonoBehaviour
+    {
+        public GameObject RowPrefab;
+
+        GameObject rowGO;
+
+        public void Reset()
+        {
+            emptyListContainers();
+        }
+
+        public void AddRow(string _title, string _value, string _subtitle = "")
+        {
+            rowGO = Instantiate(RowPrefab);
+            rowGO.transform.SetParent(transform, false);
+            rowGO.GetComponent<TableRow>().Init(_title, _value, _subtitle);
+        }
+
+        void emptyListContainers()
+        {
+            foreach (Transform t in transform) {
+                Destroy(t.gameObject);
+            }
+        }
+    }
+}
