94ddd831cbc5dd46308320e80361fe091110804e
Demigiant <daniele.giardini@gmail.com>
2016-12-01 18:27:17 +0100

    [GamesSelector] Implemented title (just numbers for now) under the bubbles

---
+++ Assets/_app/_scripts/GamesSelector/GamesSelectorUI.cs
@@ -0,0 +1,27 @@
+﻿// Author: Daniele Giardini - http://www.demigiant.com
+// Created: 2016/12/01
+
+using EA4S.Db;
+using TMPro;
+using UnityEngine;
+
+namespace EA4S
+{
+    public class GamesSelectorUI : MonoBehaviour
+    {
+        public GameObject[] Stars;
+        public TextMeshProUGUI TfImg, TfTitle;
+
+        #region Unity
+
+        void Start()
+        {
+            // Fill with data
+            JourneyPosition journeyPos = AppManager.I.Player.CurrentJourneyPosition;
+            TfTitle.text = journeyPos.ToString();
+//            PlaySessionData playSessionData = AppManager.I.DB.GetPlaySessionDataById(journeyPos.PlaySession);
+        }
+
+        #endregion
+    }
+}
