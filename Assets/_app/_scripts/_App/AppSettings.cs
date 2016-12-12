01b041f6fed8b4da36711936f21abe3ba0752039
Stefano Cecere <stefano.cecere@krur.com>
2016-12-12 23:21:36 +0100

    [build] enabled DebugPanel

--- Assets/_app/_scripts/_App/AppSettings.cs
+++ Assets/_app/_scripts/_App/AppSettings.cs
@@ -3,9 +3,9 @@ namespace EA4S
 {
     public static class AppConstants
     {
-        public const string AppVersion = "0.12.12a";
+        public const string AppVersion = "0.12.12";
         public const string DbSchemeVersion = "1";
-        public const bool DebugPanelEnabled = false;
+        public const bool DebugPanelEnabled = true;
         public const bool UseUnityAnalytics = true;
         public static bool VerboseLogging = false;
         public static bool DebugLogInserts = true;
