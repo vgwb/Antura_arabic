f3517cc0fb9f156a9c79abb75bd16ab800869e53
Michele <cat.piro@gmail.com>
2017-01-11 17:57:36 +0100

    Refactor: added EA4S.Scenes namespace. Moved HomeManager and EndManager to the /_scripts/Scenes folder. Added refactoring notes to EndManager and HomeManager.

--- Assets/_app/_scripts/Scenes/HomeManager.cs
+++ Assets/_app/_scripts/Scenes/HomeManager.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-namespace EA4S
+namespace EA4S.Scenes
 {
+    /// <summary>
+    /// Controls the _Start scene, providing an entry point for all users prior to having selected a player profile. 
+    /// </summary>
     public class HomeManager : MonoBehaviour
     {
+        // refactor: Remove the static access. The ProfileSelectorUI can directly access the HomeManager. Better yet, remove the Play() method from here and place something similar in AppManager.
         public static HomeManager I;
 
         [Header("Scene Setup")]
@@ -32,24 +35,35 @@ namespace EA4S
             LLAnimController.State = LLAnimation;
         }
 
+        /// <summary>
+        /// Start the game using the currently selected player.
+        /// </summary>
         public void Play()
         {
             Debug.Log("Play with Player ID: " + AppManager.I.Player.Id);
 
             GlobalUI.ShowPauseMenu(true);
 
+            // refactor: move this initialisation logic to the AppManager
             LogManager.I.InitNewSession();
             LogManager.I.LogInfo(InfoEvent.AppPlay, JsonUtility.ToJson(new AppInfoParameters()));
 
-            if (AppManager.I.Player.IsFirstContact()) {
+            // refactor: as this is related only to navigation, move the logic to the NavigationManager
+            if (AppManager.I.Player.IsFirstContact())
+            {
                 NavigationManager.I.GoToScene(AppScene.Intro);
-            } else {
-                if (AppManager.I.Player.MoodLastVisit == System.DateTime.Today.ToString())
-                    NavigationManager.I.GoToScene(AppScene.Map);
-                else
-                    NavigationManager.I.GoToScene(AppScene.Mood);
             }
-
+            else
+            {
+                if (AppManager.I.Player.MoodLastVisit == System.DateTime.Today.ToString())
+                {
+                    NavigationManager.I.GoToScene(AppScene.Map);
+                }
+                else
+                {
+                    NavigationManager.I.GoToScene(AppScene.Mood);
+                }
+            }
         }
     }
 }
