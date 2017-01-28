d040bd2b3e9e37d295cce8ac36fd5d28f2d9a826
Fouad Tabsh <tabshf@gmail.com>
2017-01-28 19:02:26 +0200

    [Maze] Disallow drawing outside the track.

---
+++ Assets/_games/Maze/_scripts/TrackBounds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EA4S.Minigames.Maze
+{
+    public class TrackBounds : MonoBehaviour
+    {
+        private MazeLetter mazeLetter;
+        
+        private void OnMouseOver()
+        {
+            mazeLetter.OnPointerOverTrackBounds();
+        }
+
+        public void SetMazeLetter(MazeLetter mazeLetter)
+        {
+            this.mazeLetter = mazeLetter;
+        }
+    }
+}
