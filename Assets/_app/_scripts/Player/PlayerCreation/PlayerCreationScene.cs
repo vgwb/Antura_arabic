fc39607e96c0f14e7bb8efe07c3e06b37af4614b
Stefano Cecere <stefano.cecere@krur.com>
2017-02-06 18:20:53 +0100

    PlayerCreation scene (init)

---
+++ Assets/_app/_scripts/Player/PlayerCreation/PlayerCreationScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using EA4S.Audio;
+
+namespace EA4S.Scenes
+{
+    public class PlayerCreationScene : MonoBehaviour
+    {
+        [Header("Scene Setup")]
+        public Music SceneMusic;
+
+        void Start()
+        {
+            if (SceneMusic != Music.Custom) {
+                AudioManager.I.PlayMusic(SceneMusic);
+            }
+
+        }
+
+    }
+}
