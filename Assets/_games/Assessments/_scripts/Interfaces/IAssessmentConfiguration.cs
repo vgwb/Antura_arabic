a126e13f6ef3619384a9cdb123711632e09f5e4d
PC4\Dario <oliveridario89@gmail.com>
2017-02-06 19:44:39 +0100

    Temporary Restore deprecated members in IAssessmentConfiguration-

--- Assets/_games/Assessments/_scripts/Interfaces/IAssessmentConfiguration.cs
+++ Assets/_games/Assessments/_scripts/Interfaces/IAssessmentConfiguration.cs
@@ -4,6 +4,13 @@ namespace EA4S.Assessment
 {
     public interface IAssessmentConfiguration: IGameConfiguration
     {
-        int NumberOfRounds { get; }
+        int NumberOfRounds { get; set; }
+
+        // Internal use only
+        int Rounds { get;}
+
+        // Internal use only
+        int SimultaneosQuestions { get; }
+
     }
 }
