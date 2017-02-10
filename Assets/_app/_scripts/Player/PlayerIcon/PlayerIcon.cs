48b24fdb37f21510e612bc04290c2d1b3c57629a
Demigiant <daniele.giardini@gmail.com>
2017-02-10 16:22:34 +0100

    [PlayerIcon] Added Select + Deselect method

--- Assets/_app/_scripts/Player/PlayerIcon/PlayerIcon.cs
+++ Assets/_app/_scripts/Player/PlayerIcon/PlayerIcon.cs
@@ -1,22 +1,43 @@
-﻿using DG.DeInspektor.Attributes;
+﻿using DG.DeExtensions;
+using DG.DeInspektor.Attributes;
 using EA4S.Core;
 using EA4S.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace EA4S.Profile
 {
     [RequireComponent(typeof(UIButton))]
     public class PlayerIcon : MonoBehaviour
     {
+        UIButton uiButton { get { if (fooUIButton == null) fooUIButton = this.GetComponent<UIButton>(); return fooUIButton; } }
+        UIButton fooUIButton;
+
+        #region Public
+
         public void Init(PlayerIconData playerIconData)
         {
             SetAppearance(playerIconData.Gender, playerIconData.AvatarId, playerIconData.Tint, playerIconData.IsDemoUser);
         }
 
+        [DeMethodButton("DEBUG: Select", mode = DeButtonMode.PlayModeOnly)]
+        public void Select()
+        {
+            uiButton.Toggle(true);
+        }
+
+        [DeMethodButton("DEBUG: Deselect", mode = DeButtonMode.PlayModeOnly)]
+        public void Deselect()
+        {
+            uiButton.Toggle(false);
+        }
+
+        #endregion
+
         void SetAppearance(PlayerGender gender, int avatarId, PlayerTint tint, bool isDemoUser)
         {
-            UIButton uiButton = this.GetComponent<UIButton>();
-            uiButton.ChangeDefaultColor(isDemoUser ? new Color(0.4117647f, 0.9254903f, 1f, 1f) : PlayerTintConverter.ToColor(tint));
+            Color color = isDemoUser ? new Color(0.4117647f, 0.9254903f, 1f, 1f) : PlayerTintConverter.ToColor(tint);
+            uiButton.ChangeDefaultColors(color, color.SetAlpha(0.5f));
             uiButton.Ico.sprite = isDemoUser
                 ? Resources.Load<Sprite>(AppConstants.AvatarsResourcesDir + "god")
                 : Resources.Load<Sprite>(AppConstants.AvatarsResourcesDir + gender + avatarId);
