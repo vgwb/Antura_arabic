996d2198f70b01172acdfd9766a2aa26dc6faef6
Stefano Cecere <stefano.cecere@krur.com>
2017-02-10 07:34:53 +0100

    ReservedArea filter dialog in Home Page

---
+++ Assets/_app/_scripts/ReservedArea/ReservedAreaDialog.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace EA4S.ReservedArea
+{
+    public class ReservedAreaDialog : MonoBehaviour
+    {
+        int parentLockCounter;
+
+        void OnEnable()
+        {
+            parentLockCounter = 0;
+        }
+
+        public void Close()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void OnBtnGreen()
+        {
+            parentLockCounter++;
+        }
+
+        public void OnBtnRed()
+        {
+            if (parentLockCounter == 7) {
+                UnlockParentControls();
+            } else {
+                parentLockCounter = 8; // disabling
+            }
+        }
+
+        void UnlockParentControls()
+        {
+            AppManager.I.NavigationManager.GoToAppScene(Core.AppScene.ReservedArea);
+        }
+    }
+}
