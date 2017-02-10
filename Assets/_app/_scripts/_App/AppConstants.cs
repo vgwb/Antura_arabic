beca79353734da591608625da7be920c8539def8
Davide Barbieri <davide.barbieri@ghostshark.it>
2017-02-10 18:35:51 +0100

    [Fix] Audiomanager - PopupMenu bug

--- Assets/_app/_scripts/_App/AppConstants.cs
+++ Assets/_app/_scripts/_App/AppConstants.cs
@@ -12,7 +12,7 @@
         public const bool DebugPanelEnabled = true;
         public static bool UseUnityAnalytics = false;
         public static bool VerboseLogging = true;
-        public static bool DebugLogInserts = false;
+        public static bool DebugLogInserts = true;
 
         // Urls
         public const string UrlWebsite = "http://www.antura.org";
