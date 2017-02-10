b5503676d57dc0bf070e662e131be4fe902fa1ac
Stefano Cecere <stefano.cecere@krur.com>
2017-02-10 09:51:20 +0100

    PlayerIconsData & prefab

---
+++ Assets/_app/_scripts/Player/PlayerIcon/PlayerIcon.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EA4S.Profile
+{
+    public class PlayerIcon : MonoBehaviour
+    {
+
+        public void Init(PlayerIconData _playerIconData)
+        {
+
+        }
+
+    }
+}
