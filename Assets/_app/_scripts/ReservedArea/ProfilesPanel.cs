202701ff4c9d10627e6314a8b252a7c857992680
Stefano Cecere <stefano.cecere@krur.com>
2017-02-15 05:11:08 +0100

    [res:Area] popupate Demo User

--- Assets/_app/_scripts/ReservedArea/ProfilesPanel.cs
+++ Assets/_app/_scripts/ReservedArea/ProfilesPanel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using EA4S.Core;
 using EA4S.UI;
 using EA4S.Profile;
 
@@ -11,6 +12,8 @@ namespace EA4S.ReservedArea
         public GameObject PlayerIconContainer;
         public GameObject PlayerIconPrefab;
         public GameObject ProfileCommandsContainer;
+        public GameObject pleaseWaitPanel;
+
         string SelectedPlayerId;
 
         void Start()
@@ -34,11 +37,6 @@ namespace EA4S.ReservedArea
             }
 
             List<PlayerIconData> players = AppManager.I.PlayerProfileManager.GetSavedPlayers();
-            //List<PlayerIconData> players = new List<PlayerIconData>();
-            //players.Add(new PlayerIconData("UUID-test1", 2, PlayerGender.F, PlayerTint.Green, false));
-            //players.Add(new PlayerIconData("UUID-test2", 3, PlayerGender.M, PlayerTint.Yellow, false));
-            //players.Add(new PlayerIconData("UUID-test3", 4, PlayerGender.F, PlayerTint.Red, false));
-            //players.Add(new PlayerIconData("UUID-test-DEMO", 1, PlayerGender.F, PlayerTint.Green, true));
 
             // reverse the list for RIGHT 2 LEFT layout
             players.Reverse();
@@ -48,13 +46,12 @@ namespace EA4S.ReservedArea
                 newIcon.GetComponent<PlayerIcon>().Init(player);
                 newIcon.GetComponent<UIButton>().Bt.onClick.AddListener(() => OnSelectPlayerProfile(player.Uuid));
             }
-
         }
 
         void RefreshUI()
         {
-            ProfileCommandsContainer.SetActive(SelectedPlayerId != "");
             // highlight selected profile
+            ProfileCommandsContainer.SetActive(SelectedPlayerId != "");
             foreach (Transform t in PlayerIconContainer.transform) {
                 t.GetComponent<PlayerIcon>().Select(SelectedPlayerId);
             }
@@ -98,14 +95,111 @@ namespace EA4S.ReservedArea
         public void OnCreateDemoPlayer()
         {
             Debug.Log("creating DEMO USER ");
-            AppManager.I.PlayerProfileManager.CreatePlayerProfile(10, PlayerGender.F, 1, PlayerTint.Red, true);
+            var demoUserUiid = AppManager.I.PlayerProfileManager.CreatePlayerProfile(10, PlayerGender.F, 1, PlayerTint.Red, true);
+            SelectedPlayerId = demoUserUiid;
+            AppManager.I.PlayerProfileManager.SetPlayerAsCurrentByUUID(SelectedPlayerId);
+            PopupateDemouser();
             ResetAll();
         }
+
+
 
         public void OnImportProfile()
         {
             Debug.Log("IMPORT");
         }
 
+
+        #region Demo User Helpers
+
+        void PopupateDemouser()
+        {
+            activateWaitingScreen(true);
+            Debug.Log("Cheat Mode enabled: unlocking all game data");
+            var maxJourneyPos = AppManager.I.Teacher.journeyHelper.GetFinalJourneyPosition();
+            AppManager.I.Player.SetMaxJourneyPosition(maxJourneyPos, true);
+            StartCoroutine(PopulateDatabaseWithUsefulDataCO(maxJourneyPos));
+            Rewards.RewardSystemManager.UnlockAllRewards();
+            activateWaitingScreen(false);
+        }
+
+        void activateWaitingScreen(bool status)
+        {
+            pleaseWaitPanel.gameObject.SetActive(status);
+            GlobalUI.I.BackButton.gameObject.SetActive(!status);
+        }
+
+        IEnumerator PopulateDatabaseWithUsefulDataCO(JourneyPosition targetPosition)
+        {
+
+            var logAi = AppManager.I.Teacher.logAI;
+            var fakeAppSession = LogManager.I.AppSession;
+
+            // Enable cheat mode
+            AppManager.I.GameSettings.CheatSuperDogMode = true;
+
+            // Add some mood data
+            int nMoodData = 15;
+            for (int i = 0; i < nMoodData; i++) {
+                logAi.LogMood("XXX", Random.Range(AppConstants.minimumMoodValue, AppConstants.maximumMoodValue + 1));
+                Debug.Log("Add mood " + i);
+                yield return null;
+            }
+
+
+            // Add scores for all play sessions
+            var allPlaySessionInfos = AppManager.I.Teacher.scoreHelper.GetAllPlaySessionInfo();
+            for (int i = 0; i < allPlaySessionInfos.Count; i++) {
+                if (allPlaySessionInfos[i].data.Stage <= targetPosition.Stage) {
+                    logAi.LogPlaySessionScore("XXX", allPlaySessionInfos[i].data.GetJourneyPosition(), Random.Range(1, 4), 12f);
+                    Debug.Log("Add play session score for " + allPlaySessionInfos[i].data.Id);
+                    yield return null;
+                }
+            }
+
+            // Add scores for all minigames
+            var allMiniGameInfo = AppManager.I.Teacher.scoreHelper.GetAllMiniGameInfo();
+            for (int i = 0; i < allMiniGameInfo.Count; i++) {
+                logAi.LogMiniGameScore("XXX", JourneyPosition.InitialJourneyPosition, allMiniGameInfo[i].data.Code, Random.Range(1, 4), 12f);
+                Debug.Log("Add minigame score " + i);
+                yield return null;
+            }
+
+            // Add scores for some learning data (words/letters/phrases)
+            /*var maxPlaySession = AppManager.I.Player.MaxJourneyPosition.ToString();
+            var allWordInfo = AppManager.I.Teacher.scoreHelper.GetAllWordInfo();
+            for (int i = 0; i < allWordInfo.Count; i++)
+            {
+                if (Random.value < 0.3f)
+                {
+                    var resultsList = new List<Teacher.LogAI.LearnResultParameters>();
+                    var newResult = new Teacher.LogAI.LearnResultParameters();
+                    newResult.elementId = allWordInfo[i].data.Id;
+                    newResult.table = DbTables.Words;
+                    newResult.nCorrect = Random.Range(1,5);
+                    newResult.nWrong = Random.Range(1, 5);
+                    resultsList.Add(newResult);
+                    logAi.LogLearn(fakeAppSession, maxPlaySession, MiniGameCode.Assessment_LetterForm, resultsList);
+                }
+            }
+            var allLetterInfo = AppManager.I.Teacher.scoreHelper.GetAllLetterInfo();
+            for (int i = 0; i < allLetterInfo.Count; i++)
+            {
+                if (Random.value < 0.3f)
+                {
+                    var resultsList = new List<Teacher.LogAI.LearnResultParameters>();
+                    var newResult = new Teacher.LogAI.LearnResultParameters();
+                    newResult.elementId = allLetterInfo[i].data.Id;
+                    newResult.table = DbTables.Letters;
+                    newResult.nCorrect = Random.Range(1, 5);
+                    newResult.nWrong = Random.Range(1, 5);
+                    resultsList.Add(newResult);
+                    logAi.LogLearn(fakeAppSession, maxPlaySession, MiniGameCode.Assessment_LetterForm, resultsList);
+                }
+            }*/
+
+        }
+
+        #endregion
     }
 }
