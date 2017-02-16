087a22b20bd7c1cacab4fd381d1420bf1ee1f4e5
Demigiant <daniele.giardini@gmail.com>
2017-02-16 11:08:18 +0100

    [GloblaUI - Prompt] Allows single button of onNo parameter is NULL + Callbacks are now called when the popup closes (instead of on click)

--- Assets/_app/_scripts/UI/PromptPanel.cs
+++ Assets/_app/_scripts/UI/PromptPanel.cs
@@ -18,16 +18,24 @@ namespace EA4S.UI
         public UIButton BtYes, BtNo;
 
         Action onYes, onNo;
+        Action onCloseAction;
+        float defYesX;
+        RectTransform btYesRT;
         Tween showTween;
 
         #region Unity
 
         void Awake()
         {
+            btYesRT = BtYes.GetComponent<RectTransform>();
+            defYesX = btYesRT.anchoredPosition.x;
             showTween = DOTween.Sequence().SetUpdate(true).SetAutoKill(false).Pause()
                 .Append(this.GetComponent<Image>().DOFade(0, 0.35f).From())
                 .Join(Content.DOScale(0.0001f, 0.35f).From().SetEase(Ease.OutBack))
-                .OnRewind(() => this.gameObject.SetActive(false));
+                .OnRewind(() => {
+                    this.gameObject.SetActive(false);
+                    OnClose();
+                });
 
             BtYes.Bt.onClick.AddListener(()=> OnClick(BtYes));
             BtNo.Bt.onClick.AddListener(()=> OnClick(BtNo));
@@ -48,10 +56,12 @@ namespace EA4S.UI
 
         public void Show(string _message, Action _onYes, Action _onNo)
         {
+            onCloseAction = null;
             TfMessage.text = _message.IsNullOrEmpty() ? "" : _message;
             onYes = _onYes;
             onNo = _onNo;
-
+            btYesRT.SetAnchoredPosX(_onNo == null ? 0 : defYesX);
+            BtNo.gameObject.SetActive(onNo != null);
             showTween.Restart();
             this.gameObject.SetActive(true);
         }
@@ -69,9 +79,15 @@ namespace EA4S.UI
         {
             if (showTween.IsBackwards()) return;
 
-            if (_bt == BtYes && onYes != null) onYes();
-            else if (_bt == BtNo && onNo != null) onNo();
+            if (_bt == BtYes && onYes != null) onCloseAction = onYes;
+            else if (_bt == BtNo && onNo != null) onCloseAction = onNo;
             Close();
+        }
+
+        void OnClose()
+        {
+            if (onCloseAction != null) onCloseAction();
+            onCloseAction = null;
         }
 
         #endregion
