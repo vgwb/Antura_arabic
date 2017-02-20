ea6d84a2b527dd845424472aec5a5915d1261bed
Michele <cat.piro@gmail.com>
2017-02-20 09:43:13 +0100

    Removed dependencvy of the ArabicAlphabetHelper from AppManager.I.DB

--- Assets/_manage/manage_Database/_scripts/DataParsers/WordParser.cs
+++ Assets/_manage/manage_Database/_scripts/DataParsers/WordParser.cs
@@ -32,7 +32,7 @@ namespace EA4S.Database.Management
 
         private string[] CustomParseLetters(WordData wordData, DatabaseObject db)
         {
-            var parts = ArabicAlphabetHelper.AnalyzeData(AppManager.I.DB, wordData, db);
+            var parts = ArabicAlphabetHelper.AnalyzeData(db, wordData);
 
             string[] letters = new string[parts.Count];
 
