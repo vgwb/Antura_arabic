1f6558ae3ff004f16754c05651579845f4ca79e2
Stefano Cecere <stefano.cecere@krur.com>
2017-02-23 11:48:22 +0100

    [GameSelector] LeaningBlock title (in arabic & english) + separated code

--- Assets/_app/_scripts/GamesSelector/GamesSelectorUI.cs
+++ Assets/_app/_scripts/GamesSelector/GamesSelectorUI.cs
@@ -1,9 +1,10 @@
-﻿using DG.DeExtensions;
-using EA4S.Core;
-using EA4S.Rewards;
-using TMPro;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using DG.DeExtensions;
+using EA4S.Core;
+using EA4S.UI;
+using EA4S.Database;
+using EA4S.Rewards;
 
 namespace EA4S.GamesSelector
 {
@@ -13,15 +14,19 @@ namespace EA4S.GamesSelector
     public class GamesSelectorUI : MonoBehaviour
     {
         public GameObject[] Stars;
-        public TextMeshProUGUI TfImg, TfTitle;
-
-        #region Unity
+        public TextRender TitleCode;
+        public TextRender TitleEnglish;
+        public TextRender TitleArabic;
 
         void Start()
         {
             // Fill with data
             JourneyPosition journeyPos = AppManager.I.Player.CurrentJourneyPosition;
-            TfTitle.text = journeyPos.ToString();
+            PlaySessionData playSessionData = AppManager.I.DB.GetPlaySessionDataById(journeyPos.ToStringId());
+            LearningBlockData learningBlock = AppManager.I.DB.GetLearningBlockDataById(playSessionData.Stage + "." + playSessionData.LearningBlock.ToString());
+            TitleCode.text = journeyPos.ToString();
+            TitleArabic.text = learningBlock.Title_Ar;
+            TitleEnglish.text = learningBlock.Title_En;
             if (!journeyPos.IsMinor(AppManager.I.Player.MaxJourneyPosition)) {
                 // First time playing this session: 0 stars
                 SetStars(0);
@@ -29,24 +34,18 @@ namespace EA4S.GamesSelector
                 int unlockedRewards = RewardSystemManager.GetUnlockedRewardForPlaysession(AppManager.I.Player.CurrentJourneyPosition.ToString());
                 SetStars(unlockedRewards + 1);
             }
-//            PlaySessionData playSessionData = AppManager.I.DB.GetPlaySessionDataById(journeyPos.PlaySession);
+            //            PlaySessionData playSessionData = AppManager.I.DB.GetPlaySessionDataById(journeyPos.PlaySession);
         }
-
-        #endregion
-
-        #region Methods
 
         void SetStars(int _tot)
         {
             if (_tot > 3) _tot = 3;
-            for (int i = 0; i < Stars.Length; ++i)
-            {
+            for (int i = 0; i < Stars.Length; ++i) {
                 GameObject star = Stars[i];
                 star.SetActive(i < _tot);
                 star.transform.parent.GetComponent<Image>().SetAlpha(i < _tot ? 1f : 0.3f);
             }
         }
 
-        #endregion
     }
 }
