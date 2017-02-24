dcde50303680709a3daa9bbdc5952ed5462f26d1
Stefano Cecere <stefano.cecere@krur.com>
2017-02-24 03:55:47 +0100

    [UI] english subtitles component

---
+++ Assets/_app/_scripts/UI/Specific/EnglishSubtitle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace EA4S.UI
+{
+    /// <summary>
+    /// constrain visibility of GameObject to EnglishSubtitles setting
+    /// </summary>
+    public class EnglishSubtitle : MonoBehaviour
+    {
+        void Start()
+        {
+            gameObject.SetActive(AppManager.I.GameSettings.EnglishSubtitles);
+        }
+
+    }
+}
