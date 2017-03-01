124ebc0831a9df6114499855ca45d5cf0bacdcdf
Stefano Cecere <stefano.cecere@krur.com>
2017-03-01 18:33:29 +0100

    [DB] added InitialFix to LetterData

--- Assets/_manage/manage_Database/_scripts/DataParsers/LetterParser.cs
+++ Assets/_manage/manage_Database/_scripts/DataParsers/LetterParser.cs
@@ -37,6 +37,7 @@ namespace EA4S.Database.Management
             data.Symbol_Unicode = ToString(dict["Symbol_Unicode"]);
             data.MedialFix = ToString(dict["MedialFix"]);
             data.FinalFix = ToString(dict["FinalFix"]);
+            data.InitialFix = ToString(dict["InitialFix"]);
             data.Intrinsic = ToFloat(dict["Intrinsic"]);
 
             data.Old_Isolated = ToString(dict["Old_Isolated"]);
