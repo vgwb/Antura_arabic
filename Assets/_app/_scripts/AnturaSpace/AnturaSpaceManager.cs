2aad6adbec91651dd4bbab4408c24a1bff523ae4
Davide Barbieri <davide.barbieri@ghostshark.it>
2017-03-04 12:56:28 +0100

    [AnturaSpace] KO on can't spawn bones

--- Assets/_app/_scripts/AnturaSpace/AnturaSpaceManager.cs
+++ Assets/_app/_scripts/AnturaSpace/AnturaSpaceManager.cs
@@ -86,6 +86,8 @@ namespace EA4S.AnturaSpace
                 bones.Add(bone);
                 --AppManager.I.Player.TotalNumberOfBones;
             }
+            else
+                AudioManager.I.PlaySound(Sfx.KO);
         }
 
         public bool InCustomizationMode { get; private set; }
@@ -99,7 +101,7 @@ namespace EA4S.AnturaSpace
             if (DraggingBone != null)
                 return;
 
-            if (bones.Count < MaxBonesInScene && AppManager.I.Player.TotalNumberOfBones > 0)
+            if (bones.Count <= MaxBonesInScene && AppManager.I.Player.TotalNumberOfBones > 0)
             {
                 var bone = Instantiate(BonePrefab);
                 bone.SetActive(true);
@@ -108,6 +110,10 @@ namespace EA4S.AnturaSpace
                 bones.Add(bone);
                 --AppManager.I.Player.TotalNumberOfBones;
                 bone.GetComponent<BoneBehaviour>().Drag();
+            }
+            else
+            {
+                AudioManager.I.PlaySound(Sfx.KO);
             }
         }
 
