e172b5ffc8d227140cf9d8488fafc5e7c341f0f0
Michele <cat.piro@gmail.com>
2017-03-06 15:30:56 +0100

    Added LamAlef hack that skips the .Letters of a worddata.

--- Assets/_games/Tobogan/_configurationscripts/ToboganConfiguration.cs
+++ Assets/_games/Tobogan/_configurationscripts/ToboganConfiguration.cs
@@ -68,7 +68,7 @@ namespace EA4S.Minigames.Tobogan
             switch (Variation)
             {
                 case ToboganVariation.LetterInAWord:
-                    builder = new LettersInWordQuestionBuilder(nPacks, nCorrect: nCorrect, nWrong: nWrong, parameters: builderParams);
+                    builder = new LettersInWordQuestionBuilder(nPacks, nCorrect: nCorrect, nWrong: nWrong, forceUnseparatedLetters: true, parameters: builderParams);
                     break;
                 case ToboganVariation.SunMoon:
                     builder = new WordsBySunMoonQuestionBuilder(nPacks, parameters: builderParams);
