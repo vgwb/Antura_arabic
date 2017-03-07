196f793bbcf105f3c1e031f671636dac67fc5775
Stefano Cecere <stefano.cecere@krur.com>
2017-03-07 11:06:54 +0100

    appversion (and db schemes) set to 1.0.0b7 (this will erase all previous profiles)

--- Assets/_app/_scripts/_App/AppConstants.cs
+++ Assets/_app/_scripts/_App/AppConstants.cs
@@ -11,7 +11,7 @@
         /// <summary>
         /// Version of the application. Displayed in the Home scene.
         /// </summary>
-        public const string AppVersion = "1.0b6";
+        public const string AppVersion = "1.0.0b7";
 
         #region Debug Options
 
@@ -52,13 +52,13 @@
         /// <summary>
         /// Version of the Static Database Scheme.
         /// </summary>
-        public const string StaticDbSchemeVersion = "20170306";
+        public const string StaticDbSchemeVersion = "1.0.0b7";
 
         /// <summary>
         /// Version of the MySQL Database Scheme.
         /// @note: Change with EXTREME CAUTION, as the MySQL databases are regenerated (and thus the data is removed) when a change is detected.
         /// </summary>
-        public const string DynamicDbSchemeVersion = "20170306";
+        public const string DynamicDbSchemeVersion = "1.0.0b7";
 
         // public Urls
         public const string UrlWebsite = "http://www.antura.org";
