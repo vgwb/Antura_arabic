927a4500ae37eb12fa30840af28acc8d8f147d7d
Stefano Cecere <stefano.cecere@krur.com>
2017-03-08 00:10:40 +0100

    preapring  v1.0.0b8

--- Assets/_app/_scripts/_App/AppConstants.cs
+++ Assets/_app/_scripts/_App/AppConstants.cs
@@ -11,7 +11,7 @@
         /// <summary>
         /// Version of the application. Displayed in the Home scene.
         /// </summary>
-        public const string AppVersion = "1.0.0b7a";
+        public const string AppVersion = "1.0.0b8";
 
         #region Debug Options
 
