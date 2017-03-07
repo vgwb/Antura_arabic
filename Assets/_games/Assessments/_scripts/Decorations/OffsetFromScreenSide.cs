5426764b57dcccaa16d655af68660e83cc14439e
PC4\Dario <oliveridario89@gmail.com>
2017-03-07 13:53:44 +0100

    [Assessments] Fixed Issue #413 (Z fighting on scene decorations and elements placing changing with screen ratio)

---
+++ Assets/_games/Assessments/_scripts/Decorations/OffsetFromScreenSide.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace EA4S.Assessment
+{
+    public class OffsetFromScreenSide : MonoBehaviour
+    {
+
+        public float minX;
+        public float maxX;
+
+        void Start()
+        {
+            float minRatio = 4.0f / 3.0f;
+            float maxRatio = 16.0f / 9.0f;
+            float currentRatio = Screen.width / (float)Screen.height;
+            float t = (currentRatio - minRatio) / (maxRatio - minRatio);
+            Debug.Log( "t::::: "+t);
+            Vector3 pos = transform.localPosition;
+            pos.x = Mathf.Lerp( minX, maxX, t);
+            transform.localPosition = pos;
+        }
+    }
+}
