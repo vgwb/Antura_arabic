4d235e01ff24b89582a17b8a1f48192ec40d0c7d
Demigiant <daniele.giardini@gmail.com>
2017-03-08 13:42:02 +0100

    [Map] Added StageIndicator (indicates map current stage / total stages)

---
+++ Assets/_app/_scripts/Map/MapStageIndicatorIcon.cs
@@ -0,0 +1,35 @@
+﻿
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _app._scripts.Map
+{
+    public class MapStageIndicatorIcon : MonoBehaviour
+    {
+        #region Serialized
+
+        public Color SelectedColor = Color.white;
+        public Image ColorizedImage;
+
+        #endregion
+
+        bool initialized;
+        Color defColor;
+
+        #region Public Methods
+
+        public void Select(bool doSelect)
+        {
+            if (!initialized)
+            {
+                initialized = true;
+                defColor = ColorizedImage.color;
+            }
+
+            ColorizedImage.color = doSelect ? SelectedColor : defColor;
+        }
+
+        #endregion
+    }
+}
