746544cde97b523d89b7ef2bb49d7151aeaa36ed
Stefano Cecere <stefano.cecere@krur.com>
2017-03-08 12:26:43 +0100

    TextMesh Pro: hack to adjust diacritics globally

--- Assets/TextMesh Pro mods/TextMeshPro.cs
+++ Assets/TextMesh Pro mods/TextMeshPro.cs
@@ -16,7 +16,7 @@ namespace TMPro
     [DisallowMultipleComponent]
     //[RequireComponent(typeof(TextContainer))]
     [RequireComponent(typeof(MeshRenderer))]
-    [RequireComponent(typeof(MeshFilter))] 
+    [RequireComponent(typeof(MeshFilter))]
     [AddComponentMenu("Mesh/TextMeshPro - Text")]
     [SelectionBase]
     public partial class TextMeshPro : TMP_Text, ILayoutElement
@@ -26,8 +26,7 @@ namespace TMPro
         /// <summary>
         /// Sets the Renderer's sorting Layer ID
         /// </summary>
-        public int sortingLayerID
-        {
+        public int sortingLayerID {
             get { return m_renderer.sortingLayerID; }
             set { m_renderer.sortingLayerID = value; }
         }
@@ -36,8 +35,7 @@ namespace TMPro
         /// <summary>
         /// Sets the Renderer's sorting order within the assigned layer.
         /// </summary>
-        public int sortingOrder
-        {
+        public int sortingOrder {
             get { return m_renderer.sortingOrder; }
             set { m_renderer.sortingOrder = value; }
         }
@@ -46,8 +44,7 @@ namespace TMPro
         /// <summary>
         /// Determines if the size of the text container will be adjusted to fit the text object when it is first created.
         /// </summary>
-        public override bool autoSizeTextContainer
-        {
+        public override bool autoSizeTextContainer {
             get { return m_autoSizeTextContainer; }
 
             set { if (m_autoSizeTextContainer == value) return; m_autoSizeTextContainer = value; if (m_autoSizeTextContainer) { TMP_UpdateManager.RegisterTextElementForLayoutRebuild(this); SetLayoutDirty(); } }
@@ -58,10 +55,8 @@ namespace TMPro
         /// Returns a reference to the Text Container
         /// </summary>
         [Obsolete("The TextContainer is now obsolete. Use the RectTransform instead.")]
-        public TextContainer textContainer
-        {
-            get
-            {
+        public TextContainer textContainer {
+            get {
                 return null;
             }
         }
@@ -70,26 +65,22 @@ namespace TMPro
         /// <summary>
         /// Returns a reference to the Transform
         /// </summary>
-        public new Transform transform
-        {
-            get
-            {
+        public new Transform transform {
+            get {
                 if (m_transform == null)
                     m_transform = GetComponent<Transform>();
-                
+
                 return m_transform;
             }
         }
 
 
-        #pragma warning disable 0108
+#pragma warning disable 0108
         /// <summary>
         /// Returns the rendered assigned to the text object.
         /// </summary>
-        public Renderer renderer
-        {
-            get
-            {
+        public Renderer renderer {
+            get {
                 if (m_renderer == null)
                     m_renderer = GetComponent<Renderer>();
 
@@ -101,12 +92,9 @@ namespace TMPro
         /// <summary>
         /// Returns the mesh assigned to the text object.
         /// </summary>
-        public override Mesh mesh
-        {
-            get
-            {
-                if (m_mesh == null)
-                {
+        public override Mesh mesh {
+            get {
+                if (m_mesh == null) {
                     m_mesh = new Mesh();
                     m_mesh.hideFlags = HideFlags.HideAndDontSave;
                     this.meshFilter.mesh = m_mesh;
@@ -119,10 +107,8 @@ namespace TMPro
         /// <summary>
         /// Returns the Mesh Filter of the text object.
         /// </summary>
-        public MeshFilter meshFilter
-        {
-            get
-            {
+        public MeshFilter meshFilter {
+            get {
                 if (m_meshFilter == null)
                     m_meshFilter = GetComponent<MeshFilter>();
 
@@ -134,8 +120,7 @@ namespace TMPro
         /// <summary>
         /// Sets the mask type 
         /// </summary>
-        public MaskingTypes maskType
-        {
+        public MaskingTypes maskType {
             get { return m_maskType; }
             set { m_maskType = value; SetMask(m_maskType); }
         }
@@ -236,18 +221,21 @@ namespace TMPro
         {
             if (this == null) return;
 
-            if (update == CanvasUpdate.Prelayout)
-            {
-                if (m_autoSizeTextContainer)
-                {
+            if (update == CanvasUpdate.Prelayout) {
+                if (m_autoSizeTextContainer) {
                     m_rectTransform.sizeDelta = GetPreferredValues(Mathf.Infinity, Mathf.Infinity);
                 }
-            }
-            else if (update == CanvasUpdate.PreRender)
-            {
+            } else if (update == CanvasUpdate.PreRender) {
                 this.OnPreRenderObject();
                 m_verticesAlreadyDirty = false;
                 m_layoutAlreadyDirty = false;
+
+                /// HACK to fix AdjustDiacriticPositions
+                if (EA4S.AppManager.I != null) {
+                    if (EA4S.AppManager.I.VocabularyHelper.AdjustDiacriticPositions(textInfo)) {
+                        UpdateVertexData();
+                    }
+                }
 
                 if (!m_isMaterialDirty) return;
 
@@ -378,8 +366,7 @@ namespace TMPro
         {
             int materialCount = m_textInfo.materialCount;
 
-            for (int i = 0; i < materialCount; i++)
-            {
+            for (int i = 0; i < materialCount; i++) {
                 Mesh mesh;
 
                 if (i == 0)
@@ -416,8 +403,7 @@ namespace TMPro
         {
             int materialCount = m_textInfo.materialCount;
 
-            for (int i = 0; i < materialCount; i++)
-            {
+            for (int i = 0; i < materialCount; i++) {
                 Mesh mesh;
 
                 if (i == 0)
@@ -456,8 +442,7 @@ namespace TMPro
 
             m_currentAutoSizeMode = m_enableAutoSizing;
 
-            if (m_isCalculateSizeRequired || m_rectTransform.hasChanged)
-            {
+            if (m_isCalculateSizeRequired || m_rectTransform.hasChanged) {
                 //Debug.Log("Calculating Layout Horizontal");
 
                 //m_LayoutPhase = AutoLayoutPhase.Horizontal;
@@ -468,8 +453,7 @@ namespace TMPro
 
                 //m_renderMode = TextRenderFlags.GetPreferredSizes; // Set Text to not Render and exit early once we have new width values.
 
-                if (m_enableAutoSizing)
-                {
+                if (m_enableAutoSizing) {
                     m_fontSize = m_fontSizeMax;
                 }
 
@@ -505,8 +489,7 @@ namespace TMPro
 
             //IsRectTransformDriven = true;
 
-            if (m_isCalculateSizeRequired || m_rectTransform.hasChanged)
-            {
+            if (m_isCalculateSizeRequired || m_rectTransform.hasChanged) {
                 //Debug.Log("Calculating Layout InputVertical");
 
                 //m_LayoutPhase = AutoLayoutPhase.Vertical;
@@ -517,8 +500,7 @@ namespace TMPro
 
                 //m_renderMode = TextRenderFlags.GetPreferredSizes;
 
-                if (m_enableAutoSizing)
-                {
+                if (m_enableAutoSizing) {
                     m_currentAutoSizeMode = true;
                     m_enableAutoSizing = false;
                 }
