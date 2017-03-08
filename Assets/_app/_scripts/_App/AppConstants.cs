a1215f0113d3a4941f6a3c812fc99a2f0ba797ec
Stefano Cecere <stefano.cecere@krur.com>
2017-03-08 13:21:30 +0100

    Google Play app URL

--- Assets/_app/_scripts/_App/AppConstants.cs
+++ Assets/_app/_scripts/_App/AppConstants.cs
@@ -65,7 +65,7 @@
         public const string UrlPrivacy = "http://www.antura.org/en/privacy-policy/";
         public const string UrlUploadData = "https://upload.antura.org";
         public const string UrlStoreiOSApple = "https://itunes.apple.com/us/app/antura-and-the-letters/id1210334699?ls=1&mt=8";
-        public const string UrlStoreAndroidGoogle = "";
+        public const string UrlStoreAndroidGoogle = "https://play.google.com/store/apps/details?id=org.eduapp4syria.antura";
         public const string UrlCommunityTelegram = "https://t.me/antura";
         public const string UrlCommunityFacebook = "https://www.facebook.com/antura.initiative";
 #if UNITY_EDITOR
