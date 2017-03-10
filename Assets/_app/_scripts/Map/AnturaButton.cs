89259979d75063e32dc4f8652588ed19bb67efc5
Demigiant <daniele.giardini@gmail.com>
2017-03-10 17:17:40 +0100

    [Map] Implemented NEW icon on Antura

---
+++ Assets/_app/_scripts/Map/AnturaButton.cs
@@ -0,0 +1,21 @@
+﻿
+
+using EA4S;
+using EA4S.UI;
+using UnityEngine;
+
+namespace _app._scripts.Map
+{
+    public class AnturaButton : UIButton
+    {
+        #region Unity
+
+        void Start()
+        {
+            GameObject icoNew = this.GetComponentInChildren<AnturaSpaceNewIcon>().gameObject;
+            icoNew.SetActive(AppManager.I.Player.ThereIsSomeNewReward());
+        }
+
+        #endregion
+    }
+}
