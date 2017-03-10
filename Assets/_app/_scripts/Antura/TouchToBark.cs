437e9122e0ac859fe2e7ccb52f9deed1dd5a25d2
Davide Barbieri <davide.barbieri@ghostshark.it>
2017-03-10 11:29:44 +0100

    [Home] Interactive Antura and LL

---
+++ Assets/_app/_scripts/Antura/TouchToBark.cs
@@ -0,0 +1,13 @@
+﻿using EA4S.Audio;
+using UnityEngine;
+
+namespace EA4S.Antura
+{
+    public class TouchToBark : MonoBehaviour
+    {
+        public void OnMouseDown()
+        {
+            GetComponent<AnturaAnimationController>().DoShout(() => { AudioManager.I.PlaySound(Sfx.DogBarking); });
+        }
+    }
+}
