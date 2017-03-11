0632500ed1f5a30f0377d7b54d85c911fe892aba
Brago <bragolab@gmail.com>
2017-03-11 07:35:29 +0100

    - Fix animation for skin reward in reward scene.

--- Assets/_app/_scripts/Rewards/RewardsAnimator.cs
+++ Assets/_app/_scripts/Rewards/RewardsAnimator.cs
@@ -46,47 +46,86 @@ namespace EA4S.Rewards
             rewardPackUnlockData = rewardManager.GetRewardToInstantiate();
             rotationAngleView = RewardSystemManager.GetAnturaRotationAngleViewForRewardCategory(rewardPackUnlockData.GetRewardCategory());
             newRewardInstantiatedGO = rewardManager.InstantiateReward(rewardPackUnlockData);
-            newRewardInstantiatedGO.transform.DOScale(0.00001f, 0f).SetEase(Ease.OutBack);
-            /////////
 
-            showTween = DOTween.Sequence().Pause()
-                .Append(LockClosed.DOScale(0.0001f, 0.45f).From().SetEase(Ease.OutBack))
-                .AppendInterval(0.3f)
-                .AppendCallback(() => {
-                    alarmClockSound = AudioManager.I.PlaySound(Sfx.AlarmClock);
-                })
-                .Append(LockClosed.DOShakePosition(0.8f, 40f, 16, 90, false, false))
-                .Join(LockClosed.DOShakeRotation(0.8f, new Vector3(0, 0, 70f), 16, 90, false))
-                .Join(LockClosed.DOPunchScale(Vector3.one * 0.8f, 0.4f, 20))
-                .AppendCallback(() => {
-                    LockClosed.gameObject.SetActive(false);
-                    LockOpen.gameObject.SetActive(true);
-                    if (alarmClockSound != null)
-                        alarmClockSound.Stop();
-                    AudioManager.I.PlaySound(Sfx.UIPopup);
-                })
-                .Join(LockClosed.DOScale(0.00001f, 0.4f))
-                .Join(LockOpen.DOScale(0.00001f, 0.4f).From().SetEase(Ease.OutBack))
-                .Join(Godray1.DOScale(0.00001f, 0.3f).From())
-                .AppendInterval(0.7f)
-                .Append(LockOpen.DOScale(0.00001f, 0.6f).SetEase(Ease.InBack))
-                .AppendCallback(() => {
-                    AudioManager.I.PlaySound(Sfx.Win);
-                })
-                .Join(LockOpen.DORotate(new Vector3(0, 0, 360), 0.6f, RotateMode.FastBeyond360).SetRelative().SetEase(Ease.InCubic))
-                .Join(Godray1.DOScale(0.00001f, 0.6f).SetEase(Ease.InCubic))
-                .Join(Pedestal.DOScale(0.00001f, 1f).From().SetDelay(0.5f).SetEase(Ease.OutBack))
-                .Append(Pedestal.DORotate(new Vector3(0, rotationAngleView, 0), 0.3f, RotateMode.LocalAxisAdd).SetEase(Ease.InExpo))
-                .OnComplete(() => {
-                    IsComplete = true;
-                })
-                .AppendInterval(0.1f)
-                .AppendCallback(() => playParticle())
-                .Append(newRewardInstantiatedGO.transform.DOScale(1f, 0.8f).SetEase(Ease.OutElastic))
-                .AppendInterval(0.3f)
-                .AppendCallback(() => {
-                    pedestalTween.Play();
-                });
+            if (newRewardInstantiatedGO) {
+                newRewardInstantiatedGO.transform.DOScale(0.00001f, 0f).SetEase(Ease.OutBack);
+                showTween = DOTween.Sequence().Pause()
+                    .Append(LockClosed.DOScale(0.0001f, 0.45f).From().SetEase(Ease.OutBack))
+                    .AppendInterval(0.3f)
+                    .AppendCallback(() => {
+                        alarmClockSound = AudioManager.I.PlaySound(Sfx.AlarmClock);
+                    })
+                    .Append(LockClosed.DOShakePosition(0.8f, 40f, 16, 90, false, false))
+                    .Join(LockClosed.DOShakeRotation(0.8f, new Vector3(0, 0, 70f), 16, 90, false))
+                    .Join(LockClosed.DOPunchScale(Vector3.one * 0.8f, 0.4f, 20))
+                    .AppendCallback(() => {
+                        LockClosed.gameObject.SetActive(false);
+                        LockOpen.gameObject.SetActive(true);
+                        if (alarmClockSound != null)
+                            alarmClockSound.Stop();
+                        AudioManager.I.PlaySound(Sfx.UIPopup);
+                    })
+                    .Join(LockClosed.DOScale(0.00001f, 0.4f))
+                    .Join(LockOpen.DOScale(0.00001f, 0.4f).From().SetEase(Ease.OutBack))
+                    .Join(Godray1.DOScale(0.00001f, 0.3f).From())
+                    .AppendInterval(0.7f)
+                    .Append(LockOpen.DOScale(0.00001f, 0.6f).SetEase(Ease.InBack))
+                    .AppendCallback(() => {
+                        AudioManager.I.PlaySound(Sfx.Win);
+                    })
+                    .Join(LockOpen.DORotate(new Vector3(0, 0, 360), 0.6f, RotateMode.FastBeyond360).SetRelative().SetEase(Ease.InCubic))
+                    .Join(Godray1.DOScale(0.00001f, 0.6f).SetEase(Ease.InCubic))
+                    .Join(Pedestal.DOScale(0.00001f, 1f).From().SetDelay(0.5f).SetEase(Ease.OutBack))
+                    .Append(Pedestal.DORotate(new Vector3(0, rotationAngleView, 0), 0.3f, RotateMode.LocalAxisAdd).SetEase(Ease.InExpo))
+                    .OnComplete(() => {
+                        IsComplete = true;
+                    })
+                    .AppendInterval(0.1f)
+                    .AppendCallback(() => playParticle())
+                    .Append(newRewardInstantiatedGO.transform.DOScale(1f, 0.8f).SetEase(Ease.OutElastic))
+                    .AppendInterval(0.3f)
+                    .AppendCallback(() => {
+                        pedestalTween.Play();
+                    });
+            } else {
+                showTween = DOTween.Sequence().Pause()
+                    .Append(LockClosed.DOScale(0.0001f, 0.45f).From().SetEase(Ease.OutBack))
+                    .AppendInterval(0.3f)
+                    .AppendCallback(() => {
+                        alarmClockSound = AudioManager.I.PlaySound(Sfx.AlarmClock);
+                    })
+                    .Append(LockClosed.DOShakePosition(0.8f, 40f, 16, 90, false, false))
+                    .Join(LockClosed.DOShakeRotation(0.8f, new Vector3(0, 0, 70f), 16, 90, false))
+                    .Join(LockClosed.DOPunchScale(Vector3.one * 0.8f, 0.4f, 20))
+                    .AppendCallback(() => {
+                        LockClosed.gameObject.SetActive(false);
+                        LockOpen.gameObject.SetActive(true);
+                        if (alarmClockSound != null)
+                            alarmClockSound.Stop();
+                        AudioManager.I.PlaySound(Sfx.UIPopup);
+                    })
+                    .Join(LockClosed.DOScale(0.00001f, 0.4f))
+                    .Join(LockOpen.DOScale(0.00001f, 0.4f).From().SetEase(Ease.OutBack))
+                    .Join(Godray1.DOScale(0.00001f, 0.3f).From())
+                    .AppendInterval(0.7f)
+                    .Append(LockOpen.DOScale(0.00001f, 0.6f).SetEase(Ease.InBack))
+                    .AppendCallback(() => {
+                        AudioManager.I.PlaySound(Sfx.Win);
+                    })
+                    .Join(LockOpen.DORotate(new Vector3(0, 0, 360), 0.6f, RotateMode.FastBeyond360).SetRelative().SetEase(Ease.InCubic))
+                    .Join(Godray1.DOScale(0.00001f, 0.6f).SetEase(Ease.InCubic))
+                    .Join(Pedestal.DOScale(0.00001f, 1f).From().SetDelay(0.5f).SetEase(Ease.OutBack))
+                    .Append(Pedestal.DORotate(new Vector3(0, rotationAngleView, 0), 0.3f, RotateMode.LocalAxisAdd).SetEase(Ease.InExpo))
+                    .OnComplete(() => {
+                        IsComplete = true;
+                    })
+                    .AppendInterval(0.1f)
+                    .AppendCallback(() => playParticle())
+                    .AppendInterval(0.3f)
+                    .AppendCallback(() => {
+                        pedestalTween.Play();
+                    });
+            }
 
             godraysTween = DOTween.Sequence().SetLoops(-1, LoopType.Restart)
                 .Append(Godray0.DORotate(new Vector3(0, 0, 360), Godrays360Duration, RotateMode.FastBeyond360).SetRelative().SetEase(Ease.Linear))
