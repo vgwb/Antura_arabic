fa1ff62b031d6d2a7c59713c23bce7e448ce2590
Stefano Cecere <stefano.cecere@krur.com>
2017-03-12 11:28:07 +0100

    soem comments to AppSettings

--- Assets/_app/_scripts/_App/AppSettings.cs
+++ Assets/_app/_scripts/_App/AppSettings.cs
@@ -9,16 +9,22 @@ namespace EA4S.Core
     [System.Serializable]
     public class AppSettings : ModularFramework.Modules.GlobalOptions
     {
+        // not used anymore.. but could be useful in the future
         public bool HighQualityGfx;
 
+        // the uuid of currently active player
         public string LastActivePlayerUUID;
 
-        public bool UseTestDatabase;
-
+        // to enable english subtitles (especially in the Book)
         public bool EnglishSubtitles;
 
+        // we save the current AppVersion maybe we shhould compare when installing updated versions
         public string ApplicationVersion;
 
+        // the list of saved players
         public List<PlayerIconData> SavedPlayers = new List<PlayerIconData>();
+
+        // used for development
+        public bool UseTestDatabase;
     }
 }
