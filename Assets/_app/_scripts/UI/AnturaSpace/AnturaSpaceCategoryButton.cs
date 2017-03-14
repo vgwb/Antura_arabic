afd8f87da2592e3c9622fa293ad076913d219b09
Demigiant <daniele.giardini@gmail.com>
2017-03-14 12:11:51 +0100

    [Antura Space] NEW icon now doesn't appear anymore during FIRST CONTACT

--- Assets/_app/_scripts/UI/AnturaSpace/AnturaSpaceCategoryButton.cs
+++ Assets/_app/_scripts/UI/AnturaSpace/AnturaSpaceCategoryButton.cs
@@ -29,7 +29,7 @@ namespace EA4S.UI
         public void SetAsNew(bool _isNew)
         {
             if (icoNew == null) icoNew = this.GetComponentInChildren<AnturaSpaceNewIcon>().gameObject;
-            icoNew.SetActive(_isNew);
+            icoNew.SetActive(!AppManager.I.Player.IsFirstContact() && _isNew);
         }
     }
 }
