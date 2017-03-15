c1db4889d7ab0f3574640917bbb22f61c4766a61
Stefano Cecere <stefano.cecere@krur.com>
2017-03-15 19:23:36 +0100

    [book] playerPanel init slider values

--- Assets/_app/_scripts/Book/UI/InfoTable.cs
+++ Assets/_app/_scripts/Book/UI/InfoTable.cs
@@ -5,6 +5,7 @@ namespace EA4S.Book
     public class InfoTable : MonoBehaviour
     {
         public GameObject RowPrefab;
+        public GameObject RowSliderPrefab;
 
         GameObject rowGO;
 
@@ -18,6 +19,13 @@ namespace EA4S.Book
             rowGO = Instantiate(RowPrefab);
             rowGO.transform.SetParent(transform, false);
             rowGO.GetComponent<TableRow>().Init(_titleEn, _title, _value);
+        }
+
+        public void AddSliderRow(string _titleEn, string _title, float _value, float _valueMax)
+        {
+            rowGO = Instantiate(RowSliderPrefab);
+            rowGO.transform.SetParent(transform, false);
+            rowGO.GetComponent<TableRow>().InitSlider(_titleEn, _title, _value, _valueMax);
         }
 
         void emptyListContainers()
