f1305b09d1b546dd39b114acf5c2b173041b20ee
Stefano Cecere <stefano.cecere@krur.com>
2017-03-16 01:56:51 +0100

    [reserved area] made intro text a bit little more easier

--- Assets/_app/_scripts/ReservedArea/ReservedAreaDialog.cs
+++ Assets/_app/_scripts/ReservedArea/ReservedAreaDialog.cs
@@ -40,7 +40,7 @@ namespace EA4S.ReservedArea
 
             // Number of clicks at random
             const int min_number = 4;
-            const int max_number = 8;
+            const int max_number = 7;
             firstButtonClicksTarget = Random.Range(min_number, max_number);
 
             // Update text
@@ -54,12 +54,12 @@ namespace EA4S.ReservedArea
 
             var titleLoc = LocalizationManager.GetLocalizationData("ReservedArea_Title");
             var sectionIntroLoc = LocalizationManager.GetLocalizationData("ReservedArea_SectionDescription_Intro");
-            var sectionErrorLoc= LocalizationManager.GetLocalizationData("ReservedArea_SectionDescription_Error");
+            var sectionErrorLoc = LocalizationManager.GetLocalizationData("ReservedArea_SectionDescription_Error");
 
             englishTextUI.text =
                 "<b>" + titleLoc.English + "</b>" +
                 "\n" + sectionIntroLoc.English +  //"This section is reserved for parents and guardians." +
-                "\n\nPress " + numberWord + " times the " + firstColorWord + " button, then press the " + secondColorWord + " one once." +
+                "\n\nPress <b>" + numberWord + "</b> times the <b>" + firstColorWord + "</b> button, then press the <b>" + secondColorWord + "</b> one once." +
                 "\n\n" + sectionErrorLoc.English; //"If you make an error, retry by re - accessing this panel");
 
             string numberWordArabic = AppManager.I.DB.GetWordDataById("number_0" + firstButtonClicksTarget).Arabic;
@@ -67,8 +67,8 @@ namespace EA4S.ReservedArea
             string secondColorWordArabic = colorsWordsArabic[secondButtonIndex];
 
             string arabicIntroduction = "";
-            arabicIntroduction += "<b>" + titleLoc.Arabic + "<b/> \n"; 
-            arabicIntroduction += sectionIntroLoc.Arabic + "\n\n"; 
+            arabicIntroduction += "<b>" + titleLoc.Arabic + "<b/> \n";
+            arabicIntroduction += sectionIntroLoc.Arabic + "\n\n";
             arabicIntroduction += string.Format("لفتح القفل، اضغط الزر {0} {2} مرات، ثم الزر {1} مرة واحدة", firstColorWordArabic, secondColorWordArabic, numberWordArabic);
             arabicIntroduction += "\n\n" + sectionErrorLoc.Arabic; // "\n\n في حال أخطأت، أعد المحاولة باستعمال هذه اللوحة";
 
