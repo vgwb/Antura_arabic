781ecdf27c49099c0637836b481e5d45f5c982e8
Stefano Cecere <stefano.cecere@krur.com>
2017-04-28 10:18:55 +0200

    [Reserved Area] Install Inastructions button enabled only for Android Devices

---
+++ Assets/_app/_scripts/Utility/PlatformVisibility.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlatformVisibility : MonoBehaviour
+{
+    public bool AndroidOnly;
+
+    void Start()
+    {
+        if (!Application.isEditor) {
+            if (AndroidOnly && Application.platform != RuntimePlatform.Android) {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
