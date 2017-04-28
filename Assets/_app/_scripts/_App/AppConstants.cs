be304b995ef196a2ab3ac1ffbe1224d82ca8d1d5
Stefano Cecere <stefano.cecere@krur.com>
2017-04-28 10:32:05 +0200

    preparing 1.0.4

--- Assets/_app/_scripts/_App/AppConstants.cs
+++ Assets/_app/_scripts/_App/AppConstants.cs
@@ -11,7 +11,7 @@
         /// <summary>
         /// Version of the application. Displayed in the Home scene.
         /// </summary>
-        public const string AppVersion = "1.0.4 (BETA)";
+        public const string AppVersion = "1.0.4";
 
         #region Debug Options
 
