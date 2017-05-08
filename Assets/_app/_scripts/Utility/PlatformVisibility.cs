1c1ed269cf32e2c63d65ebe77d5f108c1f075bbc
Stefano Cecere <stefano.cecere@krur.com>
2017-05-08 18:33:40 +0200

    disable Rate Button on Desktop build

--- Assets/_app/_scripts/Utility/PlatformVisibility.cs
+++ Assets/_app/_scripts/Utility/PlatformVisibility.cs
@@ -2,11 +2,15 @@
 
 public class PlatformVisibility : MonoBehaviour
 {
+    public bool MobileOnly;
     public bool AndroidOnly;
 
     void Start()
     {
         if (!Application.isEditor) {
+            if (MobileOnly && (Application.platform != RuntimePlatform.Android && Application.platform != RuntimePlatform.IPhonePlayer)) {
+                gameObject.SetActive(false);
+            }
             if (AndroidOnly && Application.platform != RuntimePlatform.Android) {
                 gameObject.SetActive(false);
             }
