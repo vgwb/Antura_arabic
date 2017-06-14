de8ca6c65459e5c4b5489dd78ce64852cd895986
Michele <cat.piro@gmail.com>
2017-06-14 15:36:08 +0200

    Player Profile Module removal: cleaned up interfaces

--- Assets/_app/_scripts/_App/ModularFramework/Modules/PlayerProfileModule/PlayerProfileModule.IPlayerProfileModule.cs
+++ Assets/_app/_scripts/_App/ModularFramework/Modules/PlayerProfileModule/PlayerProfileModule.IPlayerProfileModule.cs
@@ -1,25 +1,8 @@
 namespace EA4S.Core
 {
-    /// <summary>
-    /// Strategy interface. 
-    /// Provide All the functionalities required for any Concrete implementation of the module.
-    /// </summary>
     public interface IPlayerProfileModule : IModule<IPlayerProfileModule>
     {
-        IPlayerProfile ActivePlayer { get; set; }
         GlobalOptions Options { get; set; }
-        // Player creation
-        IPlayerProfile CreateNewPlayer(IPlayerProfile _newPlayer, IPlayerExtendedProfile _extProfile = null);
-        void DeletePlayer(string _playerId);
-        // Mod Player 
-        IPlayerProfile UpdatePlayer(IPlayerProfile _newPlayer, IPlayerExtendedProfile _extProfile = null);
-        // Change player
-        void SetActivePlayer<T>(string _playerId) where T : IPlayerProfile;
-        // Save and load
-        void SavePlayerSettings(IPlayerProfile _newPlayer, IPlayerExtendedProfile _extProfile = null);
-        IPlayerProfile LoadPlayerSettings<T>(string _playerId) where T : IPlayerProfile;
-        // Save and load Players
-        //IGlobalOptions LoadGlobalOptions<T>() where T : IGlobalOptions;
         GlobalOptions LoadGlobalOptions<T>(T _defaultOptions) where T : GlobalOptions;
         void SaveAllOptions();
     }
--- Assets/_app/_scripts/_App/ModularFramework/Modules/PlayerProfileModule/PlayerProfileModule.cs
+++ Assets/_app/_scripts/_App/ModularFramework/Modules/PlayerProfileModule/PlayerProfileModule.cs
@@ -5,10 +5,8 @@ using System.Collections.Generic;
 
 namespace EA4S.Core
 {
-    public partial class PlayerProfileModule : IPlayerProfileModule
+    public class PlayerProfileModule : IPlayerProfileModule
     {
-        public IPlayerProfile ActivePlayer { get; set; }
-
         #region IModule Implementation
 
         public IPlayerProfileModule ConcreteModuleImplementation { get; set; }
@@ -42,90 +40,6 @@ namespace EA4S.Core
         }
 
         #endregion
-
-        /// <summary>
-        /// Create new player profile, if not exist already, and save updated list of available players on PlayerPrefs.
-        /// </summary>
-        /// <param name="_newPlayer"></param>
-        /// <param name="_extProfile"></param>
-        /// <returns></returns>
-        public IPlayerProfile CreateNewPlayer(IPlayerProfile _newPlayer, IPlayerExtendedProfile _extProfile = null)
-        {
-            return null;
-        }
-
-        /// <summary>
-        /// Delete player.
-        /// </summary>
-        /// <param name="_playerId"></param>
-        public void DeletePlayer(string _playerId)
-        {
-        }
-
-        /// <summary>
-        /// Load player settings.
-        /// </summary>
-        /// <param name="_playerId"></param>
-        public IPlayerProfile LoadPlayerSettings<T>(string _playerId) where T : IPlayerProfile
-        {
-            if (PlayerPrefs.HasKey(GetStoreKeyForPlayer(_playerId)))
-            {
-                string serializableProfile = PlayerPrefs.GetString(GetStoreKeyForPlayer(_playerId));
-                IPlayerProfile returnProfile = JsonUtility.FromJson<T>(serializableProfile);
-                returnProfile.Key = _playerId;
-                return returnProfile;
-            }
-            else
-            {
-                Debug.LogFormat("Profile {0} not found.", _playerId);
-            }
-            return null;
-        }
-
-        /// <summary>
-        /// Save player settings on PlayerPrefs (do not update list of players and global options).
-        /// </summary>
-        /// <param name="_newPlayer"></param>
-        /// <param name="_extProfile"></param>
-        public void SavePlayerSettings(IPlayerProfile _newPlayer, IPlayerExtendedProfile _extProfile = null)
-        {
-            string storeKey = GetStoreKeyForPlayer(_newPlayer.Key);
-            string serializedObjs = JsonUtility.ToJson(_newPlayer);
-            if (serializedObjs != null)
-                PlayerPrefs.SetString(storeKey, serializedObjs);
-            else
-                Debug.Log("Unable to serialize player profile : " + _newPlayer.Key);
-            PlayerPrefs.Save();
-        }
-
-        /// <summary>
-        /// Set the active player.
-        /// </summary>
-        /// <param name="_playerId"></param>
-        public void SetActivePlayer<T>(string _playerId) where T : IPlayerProfile
-        {
-            IPlayerProfile pp = LoadPlayerSettings<T>(_playerId);
-            if (pp == null)
-            {
-                Debug.LogError("Player not found");
-            }
-            else
-            {
-                ActivePlayer = pp;
-            }
-        }
-
-        /// <summary>
-        /// Update player settings (and if, extended profile).
-        /// </summary>
-        /// <param name="_newPlayer"></param>
-        /// <param name="_extProfile"></param>
-        /// <returns></returns>
-        public IPlayerProfile UpdatePlayer(IPlayerProfile _newPlayer, IPlayerExtendedProfile _extProfile = null)
-        {
-            SavePlayerSettings(_newPlayer);
-            return _newPlayer;
-        }
 
         ///// <summary>
         ///// Loads the global options.
@@ -193,7 +107,7 @@ namespace EA4S.Core
         public void DeleteAllPlayerProfiles()
         {
             SaveAllOptions();
-            ActivePlayer = null;
+            //ActivePlayer = null;
         }
 
     }
