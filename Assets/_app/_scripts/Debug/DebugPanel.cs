a1e566fa6e640bca6a99c25e4c7bb06d3cbcd0c2
Stefano Cecere <stefano.cecere@krur.com>
2017-06-14 16:21:55 +0200

    go to minigames work

--- Assets/_app/_scripts/Debug/DebugPanel.cs
+++ Assets/_app/_scripts/Debug/DebugPanel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using EA4S.Core;
+using EA4S.UI;
 
 namespace EA4S.Debugging
 {
@@ -53,6 +55,37 @@ namespace EA4S.Debugging
 		{
 			clickCounter = 0;
 			Panel.SetActive(false);
+		}
+		
+		public void LaunchMinigame(MiniGameCode minigameCode)
+		{
+			if (!AppConstants.DebugStopPlayAtWrongPlaySessions || AppManager.I.Teacher.CanMiniGameBePlayedAfterMinPlaySession(new JourneyPosition(DebugManager.I.Stage, DebugManager.I.LearningBlock, DebugManager.I.PlaySession), minigameCode)) {
+				WidgetPopupWindow.I.Close();
+				DebugManager.I.LaunchMiniGame(minigameCode);
+				close();
+			} else {
+				if (AppConstants.DebugStopPlayAtWrongPlaySessions) {
+					JourneyPosition minJ = AppManager.I.JourneyHelper.GetMinimumJourneyPositionForMiniGame(minigameCode);
+					if (minJ == null) {
+						Debug.LogWarningFormat("Minigame {0} could not be selected for any PlaySession. Please check the PlaySession data table.", minigameCode);
+					} else {
+						Debug.LogErrorFormat("Minigame {0} cannot be selected this PlaySession. Min: {1}", minigameCode, minJ.ToString());
+					}
+				}
+			}
+		}
+		
+		public void GoHome()
+		{
+			// refactor: move to DebugManager
+			WidgetPopupWindow.I.Close();
+			AppManager.I.NavigationManager.GoToHome(debugMode: true);
+			close();
+		}
+		
+		public void AlphabetSong()
+		{
+			LaunchMinigame(MiniGameCode.AlphabetSong_alphabet);
 		}
 	}
 }
