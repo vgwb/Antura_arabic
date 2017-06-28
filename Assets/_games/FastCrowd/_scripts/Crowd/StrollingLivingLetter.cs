3f0afc2586d62da75ba6b2fb4353c3b9d34e48b2
Stefano Cecere <stefano.cecere@krur.com>
2017-06-28 18:07:39 +0200

    [cleaning]

--- Assets/_games/FastCrowd/_scripts/Crowd/StrollingLivingLetter.cs
+++ Assets/_games/FastCrowd/_scripts/Crowd/StrollingLivingLetter.cs
@@ -129,8 +129,6 @@ namespace EA4S.Minigames.FastCrowd
 
             if (currentData != null)
             {
-                //bool matching = GetComponent<LetterObjectView>().Model.Data.Key == currentData.Key;
-                //bool matching = GetComponent<LetterObjectView>().Data == currentData;
                 bool matching = GetComponent<LivingLetterController>().Data.Equals(currentData);
 
                 if (onDropped != null)
