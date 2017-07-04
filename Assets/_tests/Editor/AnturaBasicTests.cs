9c643bb4866c54f294adb11b850deccddc6b14aa
Stefano Cecere <stefano.cecere@krur.com>
2017-07-04 07:07:14 +0200

    tests: first UnitTest script

---
+++ Assets/_tests/Editor/AnturaBasicTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Antura.Tests
+{
+    [TestFixture]
+    [Category("Antura Basic Tests")]
+    internal class AnturaBasicTests
+    {
+ 
+        [Test]
+        public void PassingTest()
+        {
+            Assert.Pass();
+        }
+
+    }
+}
