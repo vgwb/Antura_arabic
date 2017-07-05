b2c5c2d229862620ef63bead5b3f169427f666f0
Michele <cat.piro@gmail.com>
2017-07-05 15:41:24 +0200

    tests: add DatabaseTests

---
+++ Assets/_manage/manage_Database/_scripts/Editor/DatabaseTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using EA4S.Database;
+using EA4S.Helpers;
+using NUnit.Framework;
+
+namespace EA4S.Tests
+{
+    [TestFixture]
+    public class DatabaseTests
+    {
+        [Test]
+        public void QueryStaticDB()
+        {
+            var dbManager = new DatabaseManager();
+            var allLetterData = dbManager.GetAllLetterData();
+        }
+
+        [Test]
+        public void QueryDynamicDB()
+        {
+            var dbManager = new DatabaseManager();
+            dbManager.LoadDatabaseForPlayer("TEST");
+            List<LogInfoData> list = dbManager.FindLogInfoData(x => x.Timestamp > 1000);
+        }
+
+        [Test]
+        public void InsertDynamicDB()
+        {
+            var dbManager = new DatabaseManager();
+            dbManager.LoadDatabaseForPlayer("TEST");
+            var newLogInfoData = new LogInfoData();
+            newLogInfoData.AppSession = GenericHelper.GetTimestampForNow();
+            newLogInfoData.Timestamp = GenericHelper.GetTimestampForNow();
+            newLogInfoData.Event = InfoEvent.Book;
+            newLogInfoData.AdditionalData = "test:1";
+            dbManager.Insert(newLogInfoData);
+        }
+
+    }
+}
