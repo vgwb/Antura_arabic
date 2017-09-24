b60de0f98e2fb1f8bf006a494779f54aa2f9af4a
Stefano Cecere <stefano.cecere@krur.com>
2017-09-24 14:54:43 +0200

    [gallery manager] init preview photo component

---
+++ Assets/_app/_scripts/_Core/Services/Gallery/GalleryManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Antura.Core.Services.Gallery
+{
+
+    public class GalleryManager : MonoBehaviour
+    {
+        public GameObject BtnTakePhoto;
+        public GameObject PhotoFrame;
+
+        void Start()
+        {
+
+        }
+
+    }
+}
