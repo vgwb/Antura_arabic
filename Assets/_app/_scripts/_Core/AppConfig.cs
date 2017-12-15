8e385794224698d15fc30ea7b7b8a05768a2faec
Stefano Cecere <stefano.cecere@krur.com>
2017-12-15 12:38:47 +0100

    preapring build 1.1.0beta (570)

--- Assets/_app/_scripts/_Core/AppConfig.cs
+++ Assets/_app/_scripts/_Core/AppConfig.cs
@@ -13,7 +13,7 @@ namespace Antura.Core
         /// <summary>
         /// Version of the application. Displayed in the Home scene.
         /// </summary>
-        public const string AppVersion = "1.1.0beta (569)";
+        public const string AppVersion = "1.1.0beta (570)";
 
         #region Debug Options
 
