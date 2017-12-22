59715d61167c66af7d1df6c0e9ea2ebbd4fe8fbe
Stefano Cecere <stefano.cecere@krur.com>
2017-12-22 15:11:50 +0100

    Hacks folder for Camer utilities

--- Assets/_app/_scripts/Utility/Hacks/ClearDepthBuffer.cs
+++ Assets/_app/_scripts/Utility/Hacks/ClearDepthBuffer.cs
@@ -6,6 +6,7 @@ namespace Antura.Hacks
     /// this is needed because for unkown reasons TMPro texts gets masked by some 3D objects that are BEHIND the text!
     /// waiting for Unity or TMPro to fix it, this behaviour on the camera solves the issue
     /// </summary>
+    [RequireComponent(typeof(Camera))]
     public class ClearDepthBuffer : MonoBehaviour
     {
         private void OnPostRender()
