518c48ad2df31cfa6cfd2b3d8bba1c347672245b
Stefano Cecere <stefano.cecere@krur.com>
2018-01-15 19:14:34 +0100

    preparing for version 1.1.0.582

--- Assets/_app/_scripts/_Core/AppConfig.cs
+++ Assets/_app/_scripts/_Core/AppConfig.cs
@@ -13,7 +13,7 @@ namespace Antura.Core
         /// <summary>
         /// Version of the application. Displayed in the Home scene.
         /// </summary>
-        public static Version AppVersion = new Version(1, 1, 0, 581);
+        public static Version AppVersion = new Version(1, 1, 0, 582);
 
         /// <summary>
         /// Version of the Static Database Scheme.
