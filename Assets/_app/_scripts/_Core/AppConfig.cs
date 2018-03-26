ef4b6fc949d9125730585092acc018386544ee6a
Stefano Cecere <stefano.cecere@krur.com>
2018-03-26 21:00:19 +0200

    init 1.2.1 (Kiosk edition)

--- Assets/_app/_scripts/_Core/AppConfig.cs
+++ Assets/_app/_scripts/_Core/AppConfig.cs
@@ -13,7 +13,7 @@ namespace Antura.Core
         /// <summary>
         /// Version of the application. Displayed in the Home scene.
         /// </summary>
-        public static Version AppVersion = new Version(1, 2, 0, 9);
+        public static Version AppVersion = new Version(1, 2, 1, 10);
 
         /// <summary>
         /// Version of the Static Database Scheme.
@@ -33,7 +33,7 @@ namespace Antura.Core
         /// Enabled the Advanced Debug Panel.
         /// Set to FALSE for production.
         /// </summary>
-        public static bool DebugPanelEnabledAtStartup = false;
+        public static bool DebugPanelEnabledAtStartup = true;
 
         /// <summary>
         /// Tracks common events using Unity Analytics.
@@ -45,7 +45,7 @@ namespace Antura.Core
         /// Switches on all Debug.Log calls for performance.
         /// Set to FALSE for production.
         /// </summary>
-        public static bool DebugLogEnabled = false;
+        public static bool DebugLogEnabled = true;
 
         /// <summary>
         /// Logs all MySQL database inserts.
